6c8d80516584771b50427c09f38662c7077c4cac
niko-rodrigue <niko.rodrigue@gmail.com>
2009-12-02 17:22:03 +0000

    build and test files corrected to have the 'ant test' command work without any configuration, also added the LicenseHeader and svn:keyword on the Tests files

--- src/org/sbml/jsbml/xml/test/SBML_L1VxTests.java
+++ src/org/sbml/jsbml/xml/test/SBML_L1VxTests.java
@@ -1,3 +1,33 @@
+/*
+ * $Id$
+ * $URL$
+ *
+ *
+ *==================================================================================
+ * Copyright (c) 2009 the copyright is held jointly by the individual
+ * authors. See the file AUTHORS for the list of authors.
+ *
+ * This file is part of jsbml, the pure java SBML library. Please visit
+ * http://sbml.org for more information about SBML, and http://jsbml.sourceforge.net/
+ * to get the latest version of jsbml.
+ *
+ * jsbml is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * jsbml is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jsbml.  If not, see <http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html>.
+ *
+ *===================================================================================
+ *
+ */
+
 package org.sbml.jsbml.xml.test;
 
 import static org.junit.Assert.assertTrue;
@@ -17,15 +47,23 @@ import org.sbml.jsbml.xml.SBMLReader;
 public class SBML_L1VxTests {
 	
 	
-	public static String DATA_FOLDER = System.getenv("DATA_FOLDER");
+	public static String DATA_FOLDER = null;
 	
+	static {
+		if (DATA_FOLDER == null) {
+			DATA_FOLDER = System.getenv("DATA_FOLDER"); 
+		}
+		if (DATA_FOLDER == null) {
+			DATA_FOLDER = System.getProperty("DATA_FOLDER"); 
+		}
+	}
 
 	@Before public void setUp() { 
 	}
 	
 	@Test public void readL1V2Branch() {
 		// URL fileUrl = this.getClass().getResource("./data/BIOMD0000000025.xml");
-		String fileName = DATA_FOLDER + "libsbml-test-data/l1v2-branch.xml";
+		String fileName = DATA_FOLDER + "/libsbml-test-data/l1v2-branch.xml";
 		
 		SBMLDocument doc = SBMLReader.readSBMLFile(fileName);
 		Model model = doc.getModel();
@@ -82,7 +120,7 @@ public class SBML_L1VxTests {
 	
 	@Test public void readL1V1Units() {
 		// URL fileUrl = this.getClass().getResource("./data/BIOMD0000000025.xml");
-		String fileName = DATA_FOLDER + "libsbml-test-data/l1v1-units.xml";
+		String fileName = DATA_FOLDER + "/libsbml-test-data/l1v1-units.xml";
 		
 		SBMLDocument doc = SBMLReader.readSBMLFile(fileName);
 		Model model = doc.getModel();
