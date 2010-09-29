f28f1cfb750a81e86cbfd46a12d0e846ec6faed7
draeger <andreas.draeger@uni-tuebingen.de>
2010-09-29 16:36:43 +0000

    Added our first poster about the project to the documentation folder including a short source-code example and also a screeshot under Ubuntu Linux 10.04.

---
+++ doc/posters/2010_ICSB_and_COMBINE/JSBMLvisualizer.java
@@ -0,0 +1,27 @@
+package org.sbml.simulator.gui;
+
+import javax.swing.*;
+
+import org.sbml.jsbml.SBMLDocument;
+import org.sbml.jsbml.xml.stax.SBMLReader;
+
+public class JSBMLvisualizer extends JFrame {
+
+	public JSBMLvisualizer(SBMLDocument document) {
+		super(document.isSetModel() ? document.getModel().getId() : "SBML Visualizer");
+		getContentPane().add(
+				new JScrollPane(new JTree(document),
+						JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+						JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED));
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) throws Exception {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		new JSBMLvisualizer(SBMLReader.readSBML(args[0]));
+	}
+
+}
