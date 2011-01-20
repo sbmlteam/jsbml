9cdd3faef41357dcd35a7fc8916892542deddf88
mhucka <mhucka@caltech.edu>
2011-01-20 22:41:22 +0000

    Added newly-standardized file header.

--- src/org/sbml/jsbml/validator/SBMLValidator.java
+++ src/org/sbml/jsbml/validator/SBMLValidator.java
@@ -1,31 +1,21 @@
 /*
  * $Id$
  * $URL$
+ * ----------------------------------------------------------------------------
+ * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
+ * for the latest version of JSBML and more information about SBML.
  *
+ * Copyright (C) 2009-2011 jointly by the following organizations:
+ * 1. The University of Tuebingen, Germany
+ * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ * 3. The California Institute of Technology, Pasadena, CA, USA
  *
- *==================================================================================
- * Copyright (c) 2009 the copyright is held jointly by the individual
- * authors. See the file AUTHORS for the list of authors.
- *
- * This file is part of jsbml, the pure java SBML library. Please visit
- * http://sbml.org for more information about SBML, and http://jsbml.sourceforge.net/
- * to get the latest version of jsbml.
- *
- * jsbml is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * jsbml is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with jsbml.  If not, see <http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html>.
- *
- *===================================================================================
- *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation. A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution
+ * and also available online as <http://sbml.org/Software/JSBML/License>.
+ * ----------------------------------------------------------------------------
  */
 
 package org.sbml.jsbml.validator;
