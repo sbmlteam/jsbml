3668514782f10ab397c669327d5049384587c7f7
Andreas Dräger <draeger@informatik.uni-tuebingen.de>
2011-02-02 10:37:50 +0000

    Improved the isEmpty() methods in Annotation and History and added some more methods to check if attributes are set there or not. Improved the documentation of Unit and UnitDefinition. Adapted the example file JSBMLvisualizer due to changes of the location of SBMLReader.

--- doc/posters/2010_ICSB_and_COMBINE/org/sbml/gui/JSBMLvisualizer.java
+++ doc/posters/2010_ICSB_and_COMBINE/org/sbml/gui/JSBMLvisualizer.java
@@ -1,16 +1,13 @@
 package org.sbml.gui;
 
 import javax.swing.*;
-
-import org.sbml.jsbml.SBMLDocument;
-import org.sbml.jsbml.xml.stax.SBMLReader;
+import org.sbml.jsbml.*;
 
 public class JSBMLvisualizer extends JFrame {
 
 	public JSBMLvisualizer(SBMLDocument document) {
 		super(document.isSetModel() ? document.getModel().getId() : "SBML Visualizer");
-		getContentPane().add(
-				new JScrollPane(new JTree(document),
+		getContentPane().add(new JScrollPane(new JTree(document),
 						JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 						JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -18,10 +15,10 @@ public class JSBMLvisualizer extends JFrame {
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
-
+	/** @param args Expects a valid path to an SBML file. */ 
 	public static void main(String[] args) throws Exception {
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		new JSBMLvisualizer(SBMLReader.readSBML(args[0]));
+		new JSBMLvisualizer((new SBMLReader()).readSBML(args[0]));
 	}
 
 }
