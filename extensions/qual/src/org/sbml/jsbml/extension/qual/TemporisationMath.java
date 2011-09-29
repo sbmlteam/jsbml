a16ff7c7343565f6ec65e08986854f419fb27493
niko-rodrigue <niko.rodrigue@gmail.com>
2011-09-29 15:24:06 +0000

    added the extensions skeleton classes for the adapters and for the fba and qual packages

---
+++ extensions/qual/src/org/sbml/jsbml/extension/qual/TemporisationMath.java
@@ -0,0 +1,13 @@
+package org.sbml.jsbml.extension.qual;
+
+import org.sbml.jsbml.AbstractMathContainer;
+
+public class TemporisationMath extends AbstractMathContainer {
+
+	@Override
+	public AbstractMathContainer clone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
