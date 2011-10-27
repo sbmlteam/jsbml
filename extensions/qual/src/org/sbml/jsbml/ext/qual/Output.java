64f3bc38524f979b42942d70f7fbf2c0ca8b2255
Finja Wrzodek <finja2004>
2011-10-27 09:14:19 +0000

    Added headers and implemented getters, setters, and unsetters for the variables and lists

--- extensions/qual/src/org/sbml/jsbml/ext/qual/Output.java
+++ extensions/qual/src/org/sbml/jsbml/ext/qual/Output.java
@@ -1,8 +1,35 @@
+/*
+ * $Id: Output.java 811 2011-09-27 12:36:49Z niko-rodrigue $
+ * $URL: https://jsbml.svn.sourceforge.net/svnroot/jsbml/trunk/src/org/sbml/jsbml/KineticLaw.java $
+ * ----------------------------------------------------------------------------
+ * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
+ * for the latest version of JSBML and more information about SBML.
+ *
+ * Copyright (C) 2009-2011 jointly by the following organizations:
+ * 1. The University of Tuebingen, Germany
+ * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ * 3. The California Institute of Technology, Pasadena, CA, USA
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation. A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution
+ * and also available online as <http://sbml.org/Software/JSBML/License>.
+ * ----------------------------------------------------------------------------
+ */
 package org.sbml.jsbml.ext.qual;
 
 import org.sbml.jsbml.AbstractNamedSBase;
 import org.sbml.jsbml.AbstractSBase;
-
+/**
+ * @author Nicolas Rodriguez
+ * @author Finja B&uuml;chel
+ * @author Florian Mittag
+ * @version $$Rev$$
+ * @since 0.8
+ * @date ${date}
+ * ${tags}
+ */
 public class Output extends AbstractNamedSBase {
 
 	private String qualitativeSpecies;
--- extensions/qual/src/org/sbml/jsbml/ext/qual/Transition.java
+++ extensions/qual/src/org/sbml/jsbml/ext/qual/Transition.java
@@ -1,86 +1,381 @@
+/*
+ * $Id: Transition.java 811 2011-09-27 12:36:49Z niko-rodrigue $
+ * $URL:
+ * https://jsbml.svn.sourceforge.net/svnroot/jsbml/trunk/src/org/sbml/jsbml
+ * /KineticLaw.java $
+ * ----------------------------------------------------------------------------
+ * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
+ * for the latest version of JSBML and more information about SBML.
+ * Copyright (C) 2009-2011 jointly by the following organizations:
+ * 1. The University of Tuebingen, Germany
+ * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ * 3. The California Institute of Technology, Pasadena, CA, USA
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation. A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution
+ * and also available online as <http://sbml.org/Software/JSBML/License>.
+ * ----------------------------------------------------------------------------
+ */
 package org.sbml.jsbml.ext.qual;
 
 import org.sbml.jsbml.AbstractNamedSBase;
 import org.sbml.jsbml.AbstractSBase;
 import org.sbml.jsbml.ListOf;
+import org.sbml.jsbml.PropertyUndefinedError;
 
+/**
+ * @author Nicolas Rodriguez
+ * @author Finja B&uuml;chel
+ * @author Florian Mittag
+ * @version $$Rev$$
+ * @since 0.8
+ * @date ${date}
+ *       ${tags}
+ */
 public class Transition extends AbstractNamedSBase {
 
-	private TemporisationType temporisationType;
-	
-	private ListOf<Input> listOfInputs;
-	private ListOf<Output> listOfOutputs;
-	private ListOf<FunctionTerm> listOfFunctiontTerms;
-	
-	@Override
-	public AbstractSBase clone() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  /**
+   * 
+   */
+  private static final long    serialVersionUID = 8343744362262634585L;
+  private TemporisationType    temporisationType;
+  private Sign                 sign;
+  private ListOf<Input>        listOfInputs;
+  private ListOf<Output>       listOfOutputs;
+  private ListOf<FunctionTerm> listOfFunctionTerms;
 
-	public boolean isIdMandatory() {
-		return false;
-	}
 
-	/**
-	 * @return the temporisationType
-	 */
-	public TemporisationType getTemporisationType() {
-		return temporisationType;
-	}
+  @Override
+  public AbstractSBase clone() {
+    return null;
+  }
 
-	/**
-	 * @param temporisationType the temporisationType to set
-	 */
-	public void setTemporisationType(TemporisationType temporisationType) {
-		this.temporisationType = temporisationType;
-	}
 
-	/**
-	 * @return the listOfInputs
-	 */
-	public ListOf<Input> getListOfInputs() {
-		return listOfInputs;
-	}
+  /**
+   * @return false
+   */
+  public boolean isIdMandatory() {
+    return false;
+  }
 
-	/**
-	 * @param listOfInputs the listOfInputs to set
-	 */
-	public void addInput(Input input) {
-		this.listOfInputs.add(input);
-	}
 
-	/**
-	 * @return the listOfOutputs
-	 */
-	public ListOf<Output> getListOfOutputs() {
-		return listOfOutputs;
-	}
+  /**
+   * @return false
+   */
+  public boolean isTemporisationTypeMandatory() {
+    return false;
+  }
 
-	/**
-	 * @param listOfOutputs the listOfOutputs to set
-	 */
-	public void addOutput(Output output) {
-		this.listOfOutputs.add(output);
-	}
 
-	/**
-	 * @return the listOfFunctiontTerms
-	 */
-	public ListOf<FunctionTerm> getListOfFunctiontTerms() {
-		return listOfFunctiontTerms;
-	}
+  public boolean isSetTemporisationType() {
+    return temporisationType != null;
+  }
 
-	/**
-	 * @param listOfFunctiontTerms the listOfFunctiontTerms to set
-	 */
-	public void addFunctiontTerm(FunctionTerm functiontTerm) {
-		this.listOfFunctiontTerms.add(functiontTerm);
-	}
 
-	
-	
-	// TODO : add all the necessary methods to manipulate the lists
-	
-	
+  /**
+   * @return the temporisationType
+   */
+  public TemporisationType getTemporisationType() {
+    if (isSetTemporisationType()) {
+      return temporisationType;
+    } else {
+      throw new PropertyUndefinedError(QualChangeEvent.temporisationType, this);
+    }
+  }
+
+
+  /**
+   * @param temporisationType
+   *        the temporisationType to set
+   */
+  public void setTemporisationType(TemporisationType temporisationType) {
+    TemporisationType oldType = this.temporisationType;
+    this.temporisationType = temporisationType;
+    firePropertyChange(QualChangeEvent.temporisationType, oldType,
+      this.temporisationType);
+  }
+
+
+  public boolean unsetTemporisationType() {
+    if (isSetTemporisationType()) {
+      TemporisationType oldType = this.temporisationType;
+      this.temporisationType = null;
+      firePropertyChange(QualChangeEvent.initialLevel, oldType,
+        this.temporisationType);
+      return true;
+    }
+    return false;
+  }
+
+
+  /**
+   * @return false
+   */
+  public boolean isSignMandatory() {
+    return false;
+  }
+
+
+  public boolean isSetSign() {
+    return sign != null;
+  }
+
+
+  /**
+   * @return the sign
+   */
+  public Sign getSign() {
+    if (isSetSign()) {
+      return sign;
+    } else {
+      throw new PropertyUndefinedError(QualChangeEvent.sign, this);
+    }
+  }
+
+
+  /**
+   * @param sign
+   *        the sign to set
+   */
+  public void setSign(Sign sign) {
+    Sign oldSign = this.sign;
+    this.sign = sign;
+    firePropertyChange(QualChangeEvent.sign, oldSign, this.sign);
+  }
+
+
+  /**
+   * @return true if unset the sign attribute was successful
+   */
+  public boolean unsetSign() {
+    if (isSetSign()) {
+      setSign(null);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * 
+   * @return true
+   */
+  public boolean isListOfOutputsMandatory(){
+    return true;
+  }
+  
+  public boolean isSetListOfOutputs(){
+    return listOfOutputs!=null;
+  }
+  
+  public void setListOfOutputs(ListOf<Output> loo){
+    unsetListOfOutputs();
+    this.listOfOutputs = loo;
+    setThisAsParentSBMLObject(this.listOfOutputs);
+  }
+  
+  public boolean unsetListOfOutputs() {
+    if (isSetListOfOutputs()) {
+      ListOf<Output> oldLoo = this.listOfOutputs;
+      this.listOfOutputs = null;
+      oldLoo.fireNodeRemovedEvent();
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * @return the listOfOutputs
+   */
+  public ListOf<Output> getListOfOutputs() {
+    if(!isSetListOfOutputs()){
+      listOfOutputs = new ListOf<Output>();
+    }
+    return listOfOutputs;
+  }
+
+  /**
+   * @param listOfOutputs
+   *        the listOfOutputs to set
+   */
+  public boolean addOutput(Output output) {
+    if (getListOfOutputs().add(output)) {
+      return true;
+    }
+    return false;
+  }
+  
+  /**
+   * 
+   * @param input to remove from the listOfOutputs
+   * @return true if the operation was successful
+   */
+  public boolean removeOutput(Output output) {
+    if (isSetListOfOutputs()) {
+      return listOfOutputs.remove(output);
+    }
+    return false;
+  }
+
+  /**
+   * 
+   * @param position in the listOfOutputs which should be deleted
+   * @return true if the operation was successful
+   */
+  public void removeOutput(int i) {
+    if (!isSetListOfOutputs()) {
+      throw new IndexOutOfBoundsException(Integer.toString(i));
+    }
+    listOfOutputs.remove(i);
+  }
+
+
+  /**
+   * 
+   * @return false
+   */
+  public boolean isListOfFunctionTermsMandatory(){
+    return false;
+  }
+  
+  public boolean isSetListOfFunctionTerms(){
+    return listOfFunctionTerms!=null;
+  }
+  
+  public void setListOfFunctionTerms(ListOf<FunctionTerm> loft){
+    unsetListOfFunctionTerms();
+    this.listOfFunctionTerms = loft;
+    setThisAsParentSBMLObject(this.listOfFunctionTerms);
+  }
+  
+  public boolean unsetListOfFunctionTerms() {
+    if (isSetListOfFunctionTerms()) {
+      ListOf<FunctionTerm> oldLoft = this.listOfFunctionTerms;
+      this.listOfFunctionTerms = null;
+      oldLoft.fireNodeRemovedEvent();
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * @return the listOfFunctionTerms
+   */
+  public ListOf<FunctionTerm> getListOfFunctionTerms() {
+    if(!isSetListOfFunctionTerms()){
+      listOfFunctionTerms = new ListOf<FunctionTerm>();
+    }
+    return listOfFunctionTerms;
+  }
+
+  /**
+   * @param listOfFunctionTerms
+   *        the listOfFunctionTerms to set
+   */
+  public boolean addFunctionTerm(FunctionTerm functionTerm) {
+    if (getListOfFunctionTerms().add(functionTerm)) {
+      return true;
+    }
+    return false;
+  }
+  
+  /**
+   * 
+   * @param input to remove from the listOfFunctionTerms
+   * @return true if the operation was successful
+   */
+  public boolean removeFunctionTerm(FunctionTerm functionTerm) {
+    if (isSetListOfFunctionTerms()) {
+      return listOfFunctionTerms.remove(functionTerm);
+    }
+    return false;
+  }
+
+  /**
+   * 
+   * @param position in the listOfFunctionTerms which should be deleted
+   * @return true if the operation was successful
+   */
+  public void removeFunctionTerm(int i) {
+    if (!isSetListOfFunctionTerms()) {
+      throw new IndexOutOfBoundsException(Integer.toString(i));
+    }
+    listOfFunctionTerms.remove(i);
+  }
+
+
+  /**
+   * 
+   * @return false
+   */
+  public boolean isListOfInputsMandatory(){
+    return false;
+  }
+  
+  public boolean isSetListOfInputs(){
+    return listOfInputs!=null;
+  }
+  
+  public void setListOfInputs(ListOf<Input> loi){
+    unsetListOfInputs();
+    this.listOfInputs = loi;
+    setThisAsParentSBMLObject(this.listOfInputs);
+  }
+  
+  public boolean unsetListOfInputs() {
+    if (isSetListOfInputs()) {
+      ListOf<Input> oldLoi = this.listOfInputs;
+      this.listOfInputs = null;
+      oldLoi.fireNodeRemovedEvent();
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * @return the listOfInputs
+   */
+  public ListOf<Input> getListOfInputs() {
+    if(!isSetListOfInputs()){
+      listOfInputs = new ListOf<Input>();
+    }
+    return listOfInputs;
+  }
+
+  /**
+   * @param listOfInputs
+   *        the listOfInputs to set
+   */
+  public boolean addInput(Input input) {
+    if (getListOfInputs().add(input)) {
+      return true;
+    }
+    return false;
+  }
+  
+  /**
+   * 
+   * @param input to remove from the listOfInputs
+   * @return true if the operation was successful
+   */
+  public boolean removeInput(Input input) {
+    if (isSetListOfInputs()) {
+      return listOfInputs.remove(input);
+    }
+    return false;
+  }
+
+  /**
+   * 
+   * @param position in the listOfInputs which should be deleted
+   * @return true if the operation was successful
+   */
+  public void removeInput(int i) {
+    if (!isSetListOfInputs()) {
+      throw new IndexOutOfBoundsException(Integer.toString(i));
+    }
+    listOfInputs.remove(i);
+  }
+  // TODO : add all the necessary methods to manipulate the lists
 }
