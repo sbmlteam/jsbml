4439d64c725050ca6c4eb594daa9d54f22801c68
Andreas Dräger <draeger@informatik.uni-tuebingen.de>
2012-02-22 11:17:29 +0000

    Detected a bug that made it impossible to set a kinetic law with an exisiting list of local parameters as the new kinetic law in a different reaction. JSBML always complained that all local parameters were already registered. This was the case for the previous model to which the kinetic law belonged. Hence, it was necessary to unregister all local parameters first.
    I also updated all old license headers for 2012.

--- src/org/sbml/jsbml/Reaction.java
+++ src/org/sbml/jsbml/Reaction.java
@@ -5,7 +5,7 @@
  * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
  * for the latest version of JSBML and more information about SBML.
  *
- * Copyright (C) 2009-2011 jointly by the following organizations:
+ * Copyright (C) 2009-2012 jointly by the following organizations:
  * 1. The University of Tuebingen, Germany
  * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
  * 3. The California Institute of Technology, Pasadena, CA, USA
@@ -1205,6 +1205,16 @@ public class Reaction extends AbstractNamedSBase implements CallableSBase,
 	public void setKineticLaw(KineticLaw kineticLaw) {
 		unsetKineticLaw();
 		this.kineticLaw = kineticLaw;
+		if (this.kineticLaw.isSetListOfLocalParameters()) {
+			// Clear the mapping from the kinetic law to its local parameters:
+			for (LocalParameter lp : this.kineticLaw.getListOfLocalParameters()) {
+				this.kineticLaw.registerLocalParameter(lp, true);
+			}
+			/* 
+			 * This is required because in the next step we would register
+			 * all local parameters again, which would cause an exception.
+			 */
+		}
 		setThisAsParentSBMLObject(this.kineticLaw);
 	}
 
