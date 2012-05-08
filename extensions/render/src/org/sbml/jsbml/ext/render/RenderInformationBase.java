7be6c3154d369f9e0e5e1f345c57b94dbee49e38
Eugen Netz <eugen.netz@gmail.com>
2012-05-08 12:03:50 +0000

    Render Extension: Empty classes for render extension created.

---
+++ extensions/render/src/org/sbml/jsbml/ext/render/RenderInformationBase.java
@@ -0,0 +1,77 @@
+/*
+ * $Id$
+ * $URL$
+ *
+ * ---------------------------------------------------------------------------- 
+ * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML> 
+ * for the latest version of JSBML and more information about SBML. 
+ * 
+ * Copyright (C) 2009-2011 jointly by the following organizations: 
+ * 1. The University of Tuebingen, Germany 
+ * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK 
+ * 3. The California Institute of Technology, Pasadena, CA, USA 
+ * 
+ * This library is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published by 
+ * the Free Software Foundation. A copy of the license agreement is provided 
+ * in the file named "LICENSE.txt" included with this software distribution 
+ * and also available online as <http://sbml.org/Software/JSBML/License>. 
+ * ---------------------------------------------------------------------------- 
+ */
+package org.sbml.jsbml.ext.render;
+
+import java.util.Map;
+
+import javax.swing.tree.TreeNode;
+
+import org.sbml.jsbml.ext.AbstractSBasePlugin;
+import org.sbml.jsbml.ext.SBasePlugin;
+
+/**
+ * @author Eugen Netz
+ * @author Alexander Diamantikos
+ * @author Jakob Matthes
+ * @author Jan Rudolph
+ * @version $Rev$
+ * @since 1.0
+ * @date 04.05.2012
+ */
+public class RenderInformationBase  extends AbstractSBasePlugin {
+
+  @Override
+  public SBasePlugin clone() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean getAllowsChildren() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public TreeNode getChildAt(int childIndex) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public int getChildCount() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public boolean readAttribute(String attributeName, String prefix, String value) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public Map<String, String> writeXMLAttributes() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
