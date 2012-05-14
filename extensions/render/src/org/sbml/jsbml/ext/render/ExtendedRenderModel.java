bb5787b968462ef6591b2fc6a87a2fba87383c50
enetz <eugen.netz@gmail.com>
2012-05-14 12:02:32 +0000

    Render Extension: Class hierarchy repaired.

--- extensions/render/src/org/sbml/jsbml/ext/render/ExtendedRenderModel.java
+++ extensions/render/src/org/sbml/jsbml/ext/render/ExtendedRenderModel.java
@@ -6,7 +6,7 @@
  * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML> 
  * for the latest version of JSBML and more information about SBML. 
  * 
- * Copyright (C) 2009-2011 jointly by the following organizations: 
+ * Copyright (C) 2009-2012 jointly by the following organizations: 
  * 1. The University of Tuebingen, Germany 
  * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK 
  * 3. The California Institute of Technology, Pasadena, CA, USA 
@@ -17,15 +17,18 @@
  * in the file named "LICENSE.txt" included with this software distribution 
  * and also available online as <http://sbml.org/Software/JSBML/License>. 
  * ---------------------------------------------------------------------------- 
- */
+ */ 
 package org.sbml.jsbml.ext.render;
 
 import java.util.Map;
 
 import javax.swing.tree.TreeNode;
 
+import org.sbml.jsbml.ListOf;
+import org.sbml.jsbml.Model;
 import org.sbml.jsbml.ext.AbstractSBasePlugin;
 import org.sbml.jsbml.ext.SBasePlugin;
+import org.sbml.jsbml.ext.layout.Layout;
 
 
 /**
@@ -35,45 +38,77 @@ import org.sbml.jsbml.ext.SBasePlugin;
  * @author Jan Rudolph
  * @version $Rev$
  * @since 1.0
- * @date 05.05.2012
+ * @date 14.05.2012
  */
-public class ListOfLocalRenderInformation extends AbstractSBasePlugin {
+public class ExtendedRenderModel extends AbstractSBasePlugin {
+  /**
+   * 
+   */
+  protected ListOf<LocalRenderInformation> listOfLocalRenderInformation;
+  protected ListOf<GlobalRenderInformation> listOfGlobalRenderInformation;
 
+  /**
+   * 
+   */
+  private Model model;
+
+  /* (non-Javadoc)
+   * @see org.sbml.jsbml.ext.AbstractSBasePlugin#clone()
+   */
+  
   @Override
   public SBasePlugin clone() {
     // TODO Auto-generated method stub
     return null;
   }
 
+
+  /* (non-Javadoc)
+   * @see org.sbml.jsbml.ext.SBasePlugin#getAllowsChildren()
+   */
   @Override
   public boolean getAllowsChildren() {
     // TODO Auto-generated method stub
     return false;
   }
 
+
+  /* (non-Javadoc)
+   * @see org.sbml.jsbml.ext.SBasePlugin#getChildAt(int)
+   */
   @Override
   public TreeNode getChildAt(int childIndex) {
     // TODO Auto-generated method stub
     return null;
   }
 
+
+  /* (non-Javadoc)
+   * @see org.sbml.jsbml.ext.SBasePlugin#getChildCount()
+   */
   @Override
   public int getChildCount() {
     // TODO Auto-generated method stub
     return 0;
   }
 
+
+  /* (non-Javadoc)
+   * @see org.sbml.jsbml.ext.SBasePlugin#readAttribute(java.lang.String, java.lang.String, java.lang.String)
+   */
   @Override
   public boolean readAttribute(String attributeName, String prefix, String value) {
     // TODO Auto-generated method stub
     return false;
   }
 
+
+  /* (non-Javadoc)
+   * @see org.sbml.jsbml.ext.SBasePlugin#writeXMLAttributes()
+   */
   @Override
   public Map<String, String> writeXMLAttributes() {
     // TODO Auto-generated method stub
     return null;
   }
-
-  
 }
--- extensions/render/src/org/sbml/jsbml/ext/render/Style.java
+++ extensions/render/src/org/sbml/jsbml/ext/render/Style.java
@@ -6,7 +6,7 @@
  * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML> 
  * for the latest version of JSBML and more information about SBML. 
  * 
- * Copyright (C) 2009-2011 jointly by the following organizations: 
+ * Copyright (C) 2009-2012 jointly by the following organizations: 
  * 1. The University of Tuebingen, Germany 
  * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK 
  * 3. The California Institute of Technology, Pasadena, CA, USA 
@@ -36,42 +36,5 @@ import org.sbml.jsbml.ext.SBasePlugin;
  * @since 1.0
  * @date 08.05.2012
  */
-public class Style extends AbstractSBasePlugin {
-
-  @Override
-  public SBasePlugin clone() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public boolean getAllowsChildren() {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  @Override
-  public TreeNode getChildAt(int childIndex) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public int getChildCount() {
-    // TODO Auto-generated method stub
-    return 0;
-  }
-
-  @Override
-  public boolean readAttribute(String attributeName, String prefix, String value) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  @Override
-  public Map<String, String> writeXMLAttributes() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
+public class Style extends AbstractSBase {
 }
