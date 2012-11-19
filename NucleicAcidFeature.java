515f9868f87a2a2ce94edc5b649674bb1c660648
Jakob Matthes <jakob.matthes@gmail.com>
2012-11-19 13:44:45 +0000

    Added new EPN types. Curve/edge drawing is now implemented in YLayoutBuilder.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@1091 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ NucleicAcidFeature.java
@@ -0,0 +1,41 @@
+/*
+ * $Id$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of the SysBio API library.
+ *
+ * Copyright (C) 2009-2012 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package de.zbit.sbml.layout;
+
+/**
+ * @author Jakob Matthes
+ * @version $Rev$
+ */
+public abstract class NucleicAcidFeature<T> implements SBGNNode<T> {
+
+	private boolean cloneMarker = false;
+	
+	/* (non-Javadoc)
+	 * @see de.zbit.sbml.layout.SBGNNode#setCloneMarker()
+	 */
+	public void setCloneMarker() {
+		cloneMarker = true;
+	}
+	
+	/* (non-Javadoc)
+	 * @see de.zbit.sbml.layout.SBGNNode#isSetCloneMarker()
+	 */
+	public boolean isSetCloneMarker() {
+		return cloneMarker;
+	}
+
+}
