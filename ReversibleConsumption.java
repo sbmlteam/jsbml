c1ffd216cab52a1632201c99c0df528b351a2038
Andreas Dräger <draeger@informatik.uni-tuebingen.de>
2013-03-06 15:16:09 +0000

    Unified the implementation of Layouts by shifting more code to AbstractLayoutBuilder; deleted the Mapper class RoleToSBO and replaced its functionality by a more consistent way in JSBML. Further adaptation necessary due to these changes.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@1221 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ ReversibleConsumption.java
@@ -0,0 +1,28 @@
+/*
+ * $Id$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of the SysBio API library.
+ *
+ * Copyright (C) 2009-2013 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package de.zbit.sbml.layout;
+
+/**
+ * Interface for all reversible consumption processes.
+ * 
+ * @author Andreas Dr&auml;ger
+ * @since 1.1
+ * @version $Rev$
+ */
+public interface ReversibleConsumption<T> extends SBGNArc<T> {
+	
+}
