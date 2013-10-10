0d8acf451a78f51dd415ff007df455e70b6fd98b
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2013-10-10 09:49:43 +0000

    added RemoveFromParentTest to the main Tests class so that it is included when running 'ant test'

--- core/test/org/sbml/jsbml/xml/test/Tests.java
+++ core/test/org/sbml/jsbml/xml/test/Tests.java
@@ -23,6 +23,7 @@ package org.sbml.jsbml.xml.test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.sbml.jsbml.test.RemoveFromParentTest;
 
 /**
  * 
@@ -32,7 +33,7 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 @RunWith(value=Suite.class)
 @SuiteClasses(value={SBML_L1VxTests.class, SBML_L2V1Test.class, CheckConsistencyTests.class, GetNotesStringTests.class,
-		UnregisterTests.class})
+		UnregisterTests.class, RemoveFromParentTest.class})
 public class Tests {
 
 }
