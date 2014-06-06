1854e4c55843a69cfd243d5767c30a449eb0a056
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2014-06-06 09:31:55 +0000

    added hascode and equals to Dimension and ArraysSBasePlugin + included forgotten junit test suite

--- extensions/arrays/src/org/sbml/jsbml/ext/arrays/Dimension.java
+++ extensions/arrays/src/org/sbml/jsbml/ext/arrays/Dimension.java
@@ -311,5 +311,48 @@ public class Dimension extends AbstractNamedSBase {
     
     return attributes;
   }
+
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 2707;
+    int result = super.hashCode();
+    result = prime * result + arrayDimension;
+    result = prime * result + ((size == null) ? 0 : size.hashCode());
+    return result;
+  }
+
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (!super.equals(obj)) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    Dimension other = (Dimension) obj;
+    if (arrayDimension != other.arrayDimension) {
+      return false;
+    }
+    if (size == null) {
+      if (other.size != null) {
+        return false;
+      }
+    } else if (!size.equals(other.size)) {
+      return false;
+    }
+    return true;
+  }
+  
   
 }
