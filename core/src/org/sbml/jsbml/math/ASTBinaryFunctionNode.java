bb331edab0d888a511b8bb620e4820ac2ec90e2b
Victor Kofia <victor.kofia@gmail.com>
2014-06-06 15:30:32 +0000

    Reverted types to primitives for ASTBinaryFunction. Added getChild(), setChild() methods to ASTUnaryFunction.

--- core/src/org/sbml/jsbml/math/ASTBinaryFunctionNode.java
+++ core/src/org/sbml/jsbml/math/ASTBinaryFunctionNode.java
@@ -1,30 +1,30 @@
 /*
  * $Id$
  * $URL$
- * ---------------------------------------------------------------------------- 
- * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML> 
- * for the latest version of JSBML and more information about SBML. 
+ * ----------------------------------------------------------------------------
+ * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
+ * for the latest version of JSBML and more information about SBML.
  * 
- * Copyright (C) 2009-2014  jointly by the following organizations: 
- * 1. The University of Tuebingen, Germany 
- * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK 
- * 3. The California Institute of Technology, Pasadena, CA, USA 
+ * Copyright (C) 2009-2014  jointly by the following organizations:
+ * 1. The University of Tuebingen, Germany
+ * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ * 3. The California Institute of Technology, Pasadena, CA, USA
  * 4. The University of California, San Diego, La Jolla, CA, USA
  * 5. The Babraham Institute, Cambridge, UK
  * 6. The University of Toronto, Toronto, ON, Canada
  * 
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation. A copy of the license agreement is provided 
- * in the file named "LICENSE.txt" included with this software distribution 
- * and also available online as <http://sbml.org/Software/JSBML/License>. 
- * ---------------------------------------------------------------------------- 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation. A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution
+ * and also available online as <http://sbml.org/Software/JSBML/License>.
+ * ----------------------------------------------------------------------------
  */
 package org.sbml.jsbml.math;
 
 
 /**
- * An Abstract Syntax Tree (AST) node representing a function with two 
+ * An Abstract Syntax Tree (AST) node representing a function with two
  * parameters
  * 
  * @author Victor Kofia
@@ -33,50 +33,40 @@ package org.sbml.jsbml.math;
  * @date May 30, 2014
  */
 public class ASTBinaryFunctionNode extends ASTFunction {
-  
-  /**
-   * The left child of this node.
-   */
-  private AbstractASTNode leftChild;
-  
-  /**
-   * The right child of this node
-   */
-  private AbstractASTNode rightChild;
 
   /**
    * Get the left child of this node
    * 
-   * @return AbstractASTNode leftChild
+   * @return ASTNode2 leftChild
    */
-  public AbstractASTNode getLeftChild() {
-    return leftChild;
+  public ASTNode2 getLeftChild() {
+    return getChild(0);
   }
 
   /**
    * Set the left child of this node
    * 
-   * @param AbstractASTNode leftChild
+   * @param ASTNode2 leftChild
    */
-  public void setLeftChild(AbstractASTNode leftChild) {
-    this.leftChild = leftChild;
+  public void setLeftChild(ASTNode2 leftChild) {
+    insertChild(0, leftChild);
   }
 
   /**
    * Get the right child of this node
    * 
-   * @return AbstractASTNode rightChild
+   * @return ASTNode2 rightChild
    */
-  public AbstractASTNode getRightChild() {
-    return rightChild;
+  public ASTNode2 getRightChild() {
+    return getChild(1);
   }
 
   /**
    * Set the right child of this node
    * 
-   * @param AbstractASTNode rightChild
+   * @param ASTNode2 rightChild
    */
-  public void setRightChild(AbstractASTNode rightChild) {
-    this.rightChild = rightChild;
+  public void setRightChild(ASTNode2 rightChild) {
+    insertChild(1, rightChild);
   }
 }
