591aaeda3207e8a261e6ce8fc18ca99de581237c
Ibrahim Vazirabad <yvazirabad@gmail.com>
2014-07-01 07:23:37 +0000

    Attempt to create plugin that reads CellDesigner's propertyChangeEvents

---
+++ examples/test/src/org/sbml/jsbml/gui/CDPropertyChangeVis.java
@@ -0,0 +1,105 @@
+/*
+ * $Id$
+ * $URL$
+ * ----------------------------------------------------------------------------
+ * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
+ * for the latest version of JSBML and more information about SBML.
+ * 
+ * Copyright (C) 2009-2014  jointly by the following organizations:
+ * 1. The University of Tuebingen, Germany
+ * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ * 3. The California Institute of Technology, Pasadena, CA, USA
+ * 4. The University of California, San Diego, La Jolla, CA, USA
+ * 5. The Babraham Institute, Cambridge, UK
+ * 6. Marquette University, Milwaukee, WI, USA
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation. A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution
+ * and also available online as <http://sbml.org/Software/JSBML/License>.
+ * ----------------------------------------------------------------------------
+ */
+package org.sbml.jsbml.gui;
+
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+
+
+/**
+ * @author Ibrahim Vazirabad
+ * @version $Rev$
+ * @since 1.0
+ * @date Jul 1, 2014
+ */
+public class CDPropertyChangeVis extends JFrame implements ActionListener {
+  /** Generated serial version identifier */
+  private static final long serialVersionUID = -6800051247041441688L;
+  Container contentPane= getContentPane();
+  static JTextArea viewerArea = new JTextArea(50,120);
+  static String stuff="";
+
+  public CDPropertyChangeVis() {
+    super("SBML/CD Test");
+    setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    contentPane.add(new JScrollPane(viewerArea));
+    pack();
+    setLocationRelativeTo(null);
+    setAlwaysOnTop(true);
+    setVisible(true);
+  }
+
+  public static void addSBase(String sbase)
+  {
+    String sbaseAdded="\nSBase_Added: "+sbase;
+    JOptionPane.showMessageDialog(null, new JScrollPane(new JTextArea(sbase)));
+    stuff+=sbaseAdded;
+    viewerArea.append(stuff);
+  }
+
+  public static void modelOpened(String sbase)
+  {
+    String modelOpened="\nmodel_opened: "+sbase;
+    JOptionPane.showMessageDialog(null, new JScrollPane(new JTextArea(sbase)));
+    stuff+=modelOpened;
+    viewerArea.append(stuff);
+  }
+
+  public static void modelClosed(String sbase)
+  {
+    String modelClosed="\nmodel_closed: "+sbase;
+    JOptionPane.showMessageDialog(null, new JScrollPane(new JTextArea(sbase)));
+    stuff+=modelClosed;
+    viewerArea.append(stuff);
+  }
+
+  public static void deleteSBase(String sbase)
+  {
+    String deleteSBase="\nsbase_deleted: "+sbase;
+    JOptionPane.showMessageDialog(null, new JScrollPane(new JTextArea(sbase)));
+    stuff+=deleteSBase;
+    viewerArea.append(stuff);
+  }
+
+  /** @param args Expects a valid path to an SBML file. */
+  public static void main(String[] args) throws Exception {
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    new CDPropertyChangeVis();
+  }
+
+  /* (non-Javadoc)
+   * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+   */
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    // TODO Auto-generated method stub
+
+  }
+}
---
+++ modules/celldesigner/test/org/sbml/jsbml/celldesigner/CellDesignerTest.java
@@ -0,0 +1,120 @@
+/*
+ * $Id$
+ * $URL$
+ * ----------------------------------------------------------------------------
+ * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
+ * for the latest version of JSBML and more information about SBML.
+ * 
+ * Copyright (C) 2009-2014  jointly by the following organizations:
+ * 1. The University of Tuebingen, Germany
+ * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ * 3. The California Institute of Technology, Pasadena, CA, USA
+ * 4. The University of California, San Diego, La Jolla, CA, USA
+ * 5. The Babraham Institute, Cambridge, UK
+ * 6. Marquette University, Milwaukee, WI, USA
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation. A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution
+ * and also available online as <http://sbml.org/Software/JSBML/License>.
+ * ----------------------------------------------------------------------------
+ */
+package org.sbml.jsbml.celldesigner;
+
+import java.beans.PropertyChangeEvent;
+
+import javax.swing.SwingWorker;
+import javax.xml.stream.XMLStreamException;
+
+import jp.sbi.celldesigner.plugin.PluginMenu;
+import jp.sbi.celldesigner.plugin.PluginMenuItem;
+import jp.sbi.celldesigner.plugin.PluginSBase;
+
+import org.sbml.jsbml.SBMLDocument;
+import org.sbml.jsbml.gui.CDPropertyChangeVis;
+import org.sbml.jsbml.gui.GUIErrorConsole;
+
+
+/**
+ * @author Ibrahim Vazirabad
+ * @version $Rev$
+ * @since 1.0
+ * @date Jun 30, 2014
+ */
+public class CellDesignerTest extends AbstractCellDesignerPlugin  {
+
+  public static final String ACTION = "Test CD Listeners";
+  public static final String APPLICATION_NAME = "Test the Listeners";
+
+  /**
+   * Creates a new CellDesigner plug-in with an entry in the menu bar.
+   */
+  public CellDesignerTest() {
+    super();
+    addPluginMenu();
+  }
+
+  /* (non-Javadoc)
+   * @see jp.sbi.celldesigner.plugin.CellDesignerPlug#addPluginMenu()
+   */
+  @Override
+  public void addPluginMenu() {
+    // Initializing CellDesigner's menu entries
+    PluginMenu menu = new PluginMenu(APPLICATION_NAME);
+    PluginMenuItem menuItem = new PluginMenuItem(
+      ACTION, new CellDesignerTestAction(this));
+    menuItem.setToolTipText("Tests the CD Listeners");
+    menu.add(menuItem);
+    addCellDesignerPluginMenu(menu);
+  }
+
+  /**
+   * Performs the action for which this plug-in is designed.
+   *
+   * @throws XMLStreamException If the given SBML model contains errors.
+   */
+  public void startPlugin() throws XMLStreamException {
+    // Synchronize changes from this plug-in to CellDesigner:
+    SwingWorker<SBMLDocument, Throwable> worker = new SwingWork(getReader(),getSelectedModel());
+    worker.addPropertyChangeListener(this);
+    worker.execute();
+  }
+
+  @Override
+  public void SBaseAdded(PluginSBase sbase) {
+    CDPropertyChangeVis.addSBase(sbase.toString());
+  }
+
+  @Override
+  public void modelClosed(PluginSBase sbase) {
+    CDPropertyChangeVis.modelClosed(sbase.toString());
+  }
+
+  @Override
+  public void modelOpened(PluginSBase sbase) {
+    CDPropertyChangeVis.modelOpened(sbase.toString());
+  }
+
+  @Override
+  public void SBaseDeleted(PluginSBase sbase) {
+    CDPropertyChangeVis.deleteSBase(sbase.toString());
+  }
+
+  /* (non-Javadoc)
+   * @see java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)
+   */
+  @Override
+  public void propertyChange(PropertyChangeEvent evt) {
+    if (evt.getPropertyName().equals("state") && evt.getNewValue().equals(SwingWorker.StateValue.DONE)) {
+      try {
+        SBMLDocument doc = ((SwingWork)evt.getSource()).get();
+        doc.addTreeNodeChangeListener(new PluginChangeListener(this));
+        new CDPropertyChangeVis();
+      } catch (Throwable e) {
+        new GUIErrorConsole(e);
+      }
+    }
+  }
+}
+
