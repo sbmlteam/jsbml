1deeba263e73ac81e65428ccc52c5ef7f1ba9ab7
Ibrahim Vazirabad <yvazirabad@gmail.com>
2014-07-03 00:15:53 +0000

    Adding partially working ReactionGlyph position reading (works for 1 reactant, 1 product)

--- modules/celldesigner/src/org/sbml/jsbml/celldesigner/LayoutConverter.java
+++ modules/celldesigner/src/org/sbml/jsbml/celldesigner/LayoutConverter.java
@@ -22,8 +22,15 @@
  */
 package org.sbml.jsbml.celldesigner;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
 import jp.sbi.celldesigner.plugin.PluginCompartment;
 import jp.sbi.celldesigner.plugin.PluginReaction;
+import jp.sbi.celldesigner.plugin.PluginSpecies;
 import jp.sbi.celldesigner.plugin.PluginSpeciesAlias;
 
 import org.sbml.jsbml.ext.layout.BoundingBox;
@@ -31,6 +38,7 @@ import org.sbml.jsbml.ext.layout.CompartmentGlyph;
 import org.sbml.jsbml.ext.layout.Layout;
 import org.sbml.jsbml.ext.layout.SpeciesGlyph;
 import org.sbml.jsbml.ext.layout.SpeciesReferenceGlyph;
+import org.sbml.jsbml.ext.layout.SpeciesReferenceRole;
 import org.sbml.jsbml.ext.layout.TextGlyph;
 
 
@@ -42,8 +50,8 @@ import org.sbml.jsbml.ext.layout.TextGlyph;
  */
 public class LayoutConverter {
 
-  final static double depth = 1d;
-  final static double z = 0d;
+  final static double depth  =  1d;
+  final static double z  =  0d;
 
   /**
    * Extracts Compartment Layout data from CellDesigner
@@ -52,11 +60,11 @@ public class LayoutConverter {
    */
   public static void extractLayout(PluginCompartment pCompartment, Layout layout)
   {
-    CompartmentGlyph cGlyph = layout.createCompartmentGlyph("cGlyph_" + pCompartment.getId());
+    CompartmentGlyph cGlyph  =  layout.createCompartmentGlyph("cGlyph_" + pCompartment.getId());
     cGlyph.createBoundingBox(pCompartment.getWidth(), pCompartment.getHeight(), depth, pCompartment.getX(), pCompartment.getY(), z);
 
-    TextGlyph tGlyph=layout.createTextGlyph("tGlyph_" + pCompartment.getId());
-    BoundingBox bBox = tGlyph.createBoundingBox();
+    TextGlyph tGlyph = layout.createTextGlyph("tGlyph_" + pCompartment.getId());
+    BoundingBox bBox  =  tGlyph.createBoundingBox();
     bBox.createPosition(pCompartment.getNamePositionX(), pCompartment.getNamePositionY(), z);
     tGlyph.setOriginOfText(pCompartment.getId());
   }
@@ -68,11 +76,11 @@ public class LayoutConverter {
    */
   public static void extractLayout(PluginSpeciesAlias pSpeciesAlias, Layout layout)
   {
-    SpeciesGlyph sGlyph=layout.createSpeciesGlyph("sGlyph_" + pSpeciesAlias.getSpecies().getId());
+    SpeciesGlyph sGlyph = layout.createSpeciesGlyph("sGlyph_" + pSpeciesAlias.getSpecies().getId());
     sGlyph.createBoundingBox(pSpeciesAlias.getWidth(), pSpeciesAlias.getHeight(), depth, pSpeciesAlias.getX(), pSpeciesAlias.getY(), z);
 
-    TextGlyph tGlyph=layout.createTextGlyph("tGlyph_" + pSpeciesAlias.getSpecies().getId());
-    BoundingBox bBox = tGlyph.createBoundingBox(pSpeciesAlias.getWidth(), pSpeciesAlias.getHeight(), depth);
+    TextGlyph tGlyph = layout.createTextGlyph("tGlyph_" + pSpeciesAlias.getSpecies().getId());
+    BoundingBox bBox  =  tGlyph.createBoundingBox(pSpeciesAlias.getWidth(), pSpeciesAlias.getHeight(), depth);
     //setting textGlyph position at center of speciesGlyph
     bBox.createPosition(pSpeciesAlias.getX()+pSpeciesAlias.getWidth()/2, pSpeciesAlias.getY()+pSpeciesAlias.getHeight()/2, z);
     tGlyph.setOriginOfText(pSpeciesAlias.getSpecies().getId());
@@ -85,20 +93,158 @@ public class LayoutConverter {
    */
   public static void extractLayout(PluginReaction pReaction, Layout layout)
   {
+    //arrays to help facilitate reaction positions
+    //one set of arraylists for all reactants, all products, and all modifiers
+    ArrayList<Double> reactantXCoordinates = new ArrayList<Double>();
+    ArrayList<Double> reactantYCoordinates = new ArrayList<Double>();
+    ArrayList<Double> reactantHeights = new ArrayList<Double>();
+    ArrayList<Double> reactantWidths = new ArrayList<Double>();
+
+    ArrayList<Double> productXCoordinates = new ArrayList<Double>();
+    ArrayList<Double> productYCoordinates = new ArrayList<Double>();
+    ArrayList<Double> productHeights = new ArrayList<Double>();
+    ArrayList<Double> productWidths = new ArrayList<Double>();
+
+    ArrayList<Double> modifierXCoordinates = new ArrayList<Double>();
+    ArrayList<Double> modifierYCoordinates = new ArrayList<Double>();
+    ArrayList<Double> modifierHeights = new ArrayList<Double>();
+    ArrayList<Double> modifierWidths = new ArrayList<Double>();
+
+    //create ReactionGlyph
     layout.createReactionGlyph("rGlyph_"+pReaction.getId());
-    for (int i=0;i<pReaction.getNumReactants();i++)
+
+    //create SpeciesReferenceGlyphs for reactants
+    for (int i = 0;i<pReaction.getNumReactants();i++)
     {
-      SpeciesReferenceGlyph srGlyph=new SpeciesReferenceGlyph("srGlyph_"+pReaction.getId()+"_"+i);
-      srGlyph.setSpeciesGlyph("sGlyph_"+pReaction.getReactant(i).getSpeciesInstance().getId());
+      SpeciesReferenceGlyph srGlyph = new SpeciesReferenceGlyph("srGlyphReactant_"+pReaction.getId()+"_"+
+          pReaction.getReactant(i).getSpeciesInstance().getId());
+      srGlyph.setRole(SpeciesReferenceRole.SUBSTRATE);
+
+      PluginSpecies pSpecies = pReaction.getReactant(i).getSpeciesInstance();
+      reactantXCoordinates.add(pSpecies.getSpeciesAlias(0).getX());
+      reactantYCoordinates.add(pSpecies.getSpeciesAlias(0).getY());
+      reactantHeights.add(pSpecies.getSpeciesAlias(0).getHeight());
+      reactantWidths.add(pSpecies.getSpeciesAlias(0).getWidth());
+      srGlyph.setSpeciesGlyph("sGlyph_"+pSpecies.getId());
       layout.getReactionGlyph("rGlyph_"+pReaction.getId()).addSpeciesReferenceGlyph(srGlyph);
     }
-    for (int i=0;i<pReaction.getNumProducts();i++)
+
+    //create SpeciesReferenceGlyphs for products
+    for (int i = 0;i<pReaction.getNumProducts();i++)
     {
-      SpeciesReferenceGlyph srGlyph=new SpeciesReferenceGlyph("srGlyph_"+pReaction.getId()+"__"+i);
-      srGlyph.setSpeciesGlyph("sGlyph_"+pReaction.getProduct(i).getSpeciesInstance().getId());
+      SpeciesReferenceGlyph srGlyph = new SpeciesReferenceGlyph("srGlyphProduct_" + pReaction.getId() + "_" +
+          pReaction.getProduct(i).getSpeciesInstance().getId());
+      srGlyph.setRole(SpeciesReferenceRole.PRODUCT);
+
+      PluginSpecies pSpecies = pReaction.getProduct(i).getSpeciesInstance();
+      productXCoordinates.add(pSpecies.getSpeciesAlias(0).getX());
+      productYCoordinates.add(pSpecies.getSpeciesAlias(0).getY());
+      productHeights.add(pSpecies.getSpeciesAlias(0).getHeight());
+      productWidths.add(pSpecies.getSpeciesAlias(0).getWidth());
+
+      srGlyph.setSpeciesGlyph("sGlyph_"+pSpecies.getId());
       layout.getReactionGlyph("rGlyph_"+pReaction.getId()).addSpeciesReferenceGlyph(srGlyph);
     }
-    TextGlyph tGlyph=layout.createTextGlyph("tGlyph_" + pReaction.getId());
+
+    //create SpeciesReferenceGlyphs for modifiers
+    for (int i = 0;i<pReaction.getNumModifiers();i++)
+    {
+      SpeciesReferenceGlyph srGlyph = new SpeciesReferenceGlyph("srGlyphModifier_" + pReaction.getId()+ "_" +
+          pReaction.getModifier(i).getSpeciesInstance().getId());
+      srGlyph.setRole(SpeciesReferenceRole.MODIFIER);
+
+      PluginSpecies pSpecies = pReaction.getModifier(i).getSpeciesInstance();
+      modifierXCoordinates.add(pSpecies.getSpeciesAlias(0).getX());
+      modifierYCoordinates.add(pSpecies.getSpeciesAlias(0).getY());
+      modifierHeights.add(pSpecies.getSpeciesAlias(0).getHeight());
+      modifierWidths.add(pSpecies.getSpeciesAlias(0).getWidth());
+      srGlyph.setSpeciesGlyph("sGlyph_"+pSpecies.getId());
+      layout.getReactionGlyph("rGlyph_"+pReaction.getId()).addSpeciesReferenceGlyph(srGlyph);
+    }
+
+    //start calculating reactionGlyph positioning
+    double reactionWidth = 0, reactionHeight = 0;
+    double reactionXCoordinate = 0, reactionYCoordinate = 0;
+
+    //number of reactants and products are only 1
+    if (reactantXCoordinates.size() == 1 && productXCoordinates.size() == 1)
+    {
+      if (reactantXCoordinates.get(0)<productXCoordinates.get(0))
+      {
+        reactionXCoordinate=reactantXCoordinates.get(0)+reactantWidths.get(0);
+        reactionYCoordinate=reactantYCoordinates.get(0)+(reactantHeights.get(0)/2);
+        reactionWidth=productXCoordinates.get(0)-reactionXCoordinate;
+        reactionHeight=Math.abs((productYCoordinates.get(0)+productHeights.get(0)/2)-reactionYCoordinate);
+      }
+      else if (reactantXCoordinates.get(0)>=productXCoordinates.get(0))
+      {
+        reactionXCoordinate=reactantXCoordinates.get(0);
+        reactionYCoordinate=reactantYCoordinates.get(0)+(reactantHeights.get(0)/2);
+        reactionWidth=reactionXCoordinate-(productXCoordinates.get(0)+productWidths.get(0));
+        reactionHeight=Math.abs((productYCoordinates.get(0)+productHeights.get(0)/2)-reactionYCoordinate);
+      }
+    }
+
+    //multiple reactants, one product
+    else if (reactantXCoordinates.size()>1 && productXCoordinates.size() == 1)
+    {
+      reactionXCoordinate = 9999;
+      for (double d:reactantXCoordinates) {
+        if (d<reactionXCoordinate) {
+          reactionXCoordinate = d;
+        }
+        reactionXCoordinate = Math.min(reactionXCoordinate, productXCoordinates.get(0))+reactantWidths.get(0);
+        reactionWidth = Math.abs(reactionXCoordinate-productXCoordinates.get(0));
+      }
+      for (double d:reactantYCoordinates)
+      {
+        reactionYCoordinate = 9999;
+        if (d<reactionYCoordinate) {
+          reactionYCoordinate = d;
+        }
+        reactionYCoordinate = Math.max(reactionYCoordinate, productYCoordinates.get(0));
+        reactionHeight = Math.abs(reactionYCoordinate-productYCoordinates.get(0));
+      }
+    }
+    //one reactant, multiple products
+    else if (reactantXCoordinates.size() == 1 && productXCoordinates.size()>1)
+    {
+      reactionXCoordinate = 9999;
+      for (double d:productXCoordinates) {
+        if (d<reactionXCoordinate) {
+          reactionXCoordinate = d;
+        }
+        reactionXCoordinate = Math.max(reactionXCoordinate, reactantXCoordinates.get(0));
+        reactionWidth = Math.abs(reactionXCoordinate-reactantXCoordinates.get(0));
+      }
+      for (double d:productYCoordinates)
+      {
+        reactionYCoordinate = 9999;
+        if (d<reactionYCoordinate) {
+          reactionYCoordinate = d;
+        }
+        reactionYCoordinate = Math.max(reactionYCoordinate, reactantYCoordinates.get(0));
+        reactionHeight  =  Math.abs(reactionYCoordinate-reactantYCoordinates.get(0));
+      }
+    }
+
+    //adding modifier height/width to reaction
+    if (modifierYCoordinates.size() == 1)
+    {
+      double modifierHeight = Math.abs(reactionYCoordinate-modifierYCoordinates.get(0));
+      reactionHeight += modifierHeight;
+      double modifierWidth = Math.abs(reactionXCoordinate-modifierXCoordinates.get(0));
+      reactionWidth += modifierWidth;
+    }
+
+    //for debugging purposes
+    JOptionPane.showMessageDialog(null, new JScrollPane(new JTextArea(pReaction.getId()+" ID\n"+reactionXCoordinate+" X\n"+reactionYCoordinate
+      +" Y\n"+reactionHeight+" Height\n"+reactionWidth+" Width\n"+productXCoordinates.get(0)+" productX\n"+productYCoordinates.get(0)
+      +" productY\n"+reactantXCoordinates.get(0)+" reactionX\n"+reactantYCoordinates.get(0)+" reactY\n")));
+    layout.getReactionGlyph("rGlyph_"+pReaction.getId()).createBoundingBox(reactionWidth, reactionHeight, depth, reactionXCoordinate, reactionYCoordinate, z);
+
+    TextGlyph tGlyph = layout.createTextGlyph("tGlyph_" + pReaction.getId());
+    tGlyph.createBoundingBox(15, 10, depth, reactionXCoordinate-10, reactionYCoordinate-10, z);
     tGlyph.setOriginOfText(pReaction.getId());
   }
 }
