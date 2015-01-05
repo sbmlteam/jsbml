d33f4028ec1c8ae4e95f0a3e4e0a66f8e2635563
Andreas Dräger <draeger@informatik.uni-tuebingen.de>
2015-01-05 04:50:45 +0000

    Updated license headers to 2015. Happy new year to all!

--- core/doc/user_guide/src/org/sbml/jsbml/celldesigner/cdplugin_example.java
+++ core/doc/user_guide/src/org/sbml/jsbml/celldesigner/cdplugin_example.java
@@ -1,3 +1,24 @@
+/*
+ * $Id$
+ * $URL$
+ * ----------------------------------------------------------------------------
+ * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
+ * for the latest version of JSBML and more information about SBML.
+ *
+ * Copyright (C) 2009-2015 jointly by the following organizations:
+ * 1. The University of Tuebingen, Germany
+ * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ * 3. The California Institute of Technology, Pasadena, CA, USA
+ * 4. The University of California, San Diego, La Jolla, CA, USA
+ * 5. The Babraham Institute, Cambridge, UK
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation. A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution
+ * and also available online as <http://sbml.org/Software/JSBML/License>.
+ * ----------------------------------------------------------------------------
+ */
 package org.sbml.jsbml.celldesigner;
 
 import javax.swing.*;
@@ -8,45 +29,52 @@ import org.sbml.jsbml.gui.*;
 /** A very simple implementation of a plugin for CellDesigner. */
 public class SimpleCellDesignerPlugin extends CellDesignerPlugin {
 
-    public static final String ACTION = "Display full model tree";
-    public static final String APPLICATION_NAME = "Simple Plugin";
+  public static final String ACTION = "Display full model tree";
+  public static final String APPLICATION_NAME = "Simple Plugin";
 
-    /** Creates a new CellDesigner plugin with an entry in the menu bar. */
-    public SimpleCellDesignerPlugin() {
-        super();
-        try {
-            System.out.printf("\n\nLoading %s\n\n", APPLICATION_NAME);
-            SimpleCellDesignerPluginAction action = new SimpleCellDesignerPluginAction(this);
-            PluginMenu menu = new PluginMenu(APPLICATION_NAME);
-            PluginMenuItem menuItem = new PluginMenuItem(ACTION, action);
-            menuItem.setName("some_id");
-            menu.add(menuItem);
-            addCellDesignerPluginMenu(menu);
-        } catch (Exception exc) {
-            exc.printStackTrace();
-        }
+  /** Creates a new CellDesigner plugin with an entry in the menu bar. */
+  public SimpleCellDesignerPlugin() {
+    super();
+    try {
+      System.out.printf("\n\nLoading %s\n\n", APPLICATION_NAME);
+      SimpleCellDesignerPluginAction action = new SimpleCellDesignerPluginAction(this);
+      PluginMenu menu = new PluginMenu(APPLICATION_NAME);
+      PluginMenuItem menuItem = new PluginMenuItem(ACTION, action);
+      menuItem.setName("some_id");
+      menu.add(menuItem);
+      addCellDesignerPluginMenu(menu);
+    } catch (Exception exc) {
+      exc.printStackTrace();
     }
+  }
 
-    /** This method is to be called by our CellDesignerPluginAction. */
-    public void startPlugin() {
-        PluginSBMLReader reader
-            = new PluginSBMLReader(getSelectedModel(), SBO.getDefaultPossibleEnzymes());
+  /** This method is to be called by our CellDesignerPluginAction. */
+  public void startPlugin() {
+    PluginSBMLReader reader
+    = new PluginSBMLReader(getSelectedModel(), SBO.getDefaultPossibleEnzymes());
 
-        // In CellDesigner, the SBMLDocument object is not accessible, so we must create a new one
-        // after obtaining the model from the reader.
+    // In CellDesigner, the SBMLDocument object is not accessible, so we must create a new one
+    // after obtaining the model from the reader.
 
-        Model model = reader.getModel();
-        SBMLDocument doc = new SBMLDocument(model.getLevel(), model.getVersion());
-        doc.setModel(model);
-        new JSBMLvisualizer(doc);
-    }
+    Model model = reader.getModel();
+    SBMLDocument doc = new SBMLDocument(model.getLevel(), model.getVersion());
+    doc.setModel(model);
+    new JSBMLvisualizer(doc);
+  }
 
-    // Include also methods from superclass, not needed in this example.
-    public void addPluginMenu() { }
-    public void modelClosed(PluginSBase psb) { }
-    public void modelOpened(PluginSBase psb) { }
-    public void modelSelectChanged(PluginSBase psb) { }
-    public void SBaseAdded(PluginSBase psb) { }
-    public void SBaseChanged(PluginSBase psb) { }
-    public void SBaseDeleted(PluginSBase psb) { }
+  // Include also methods from superclass, not needed in this example.
+  @Override
+  public void addPluginMenu() { }
+  @Override
+  public void modelClosed(PluginSBase psb) { }
+  @Override
+  public void modelOpened(PluginSBase psb) { }
+  @Override
+  public void modelSelectChanged(PluginSBase psb) { }
+  @Override
+  public void SBaseAdded(PluginSBase psb) { }
+  @Override
+  public void SBaseChanged(PluginSBase psb) { }
+  @Override
+  public void SBaseDeleted(PluginSBase psb) { }
 }
--- core/src/org/sbml/jsbml/xml/parsers/AnnotationReader.java
+++ core/src/org/sbml/jsbml/xml/parsers/AnnotationReader.java
@@ -5,9 +5,10 @@
  * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
  * for the latest version of JSBML and more information about SBML.
  *
- * Copyright (C) 2009-2012 jointly by the following organizations:
+ * Copyright (C) 2009-2015 jointly by the following organizations:
  * 1. The University of Tuebingen, Germany
  * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ * 3. The California Institute of Technology, Pasadena, CA, USA
  * 4. The University of California, San Diego, La Jolla, CA, USA
  * 5. The Babraham Institute, Cambridge, UK
  * 
