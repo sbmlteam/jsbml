fe11ae585ad7ee1b1219c015ad2cbce35f443bfd
draeger <andreas.draeger@uni-tuebingen.de>
2015-01-05 04:50:45 +0000

    Updated license headers to 2015. Happy new year to all!

--- core/src/org/sbml/jsbml/text/parser/SimpleCharStream.java
+++ core/src/org/sbml/jsbml/text/parser/SimpleCharStream.java
@@ -7,7 +7,7 @@
  * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
  * for the latest version of JSBML and more information about SBML.
  *
- * Copyright (C) 2009-2014 jointly by the following organizations:
+ * Copyright (C) 2009-2015 jointly by the following organizations:
  * 1. The University of Tuebingen, Germany
  * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
  * 3. The California Institute of Technology, Pasadena, CA, USA
