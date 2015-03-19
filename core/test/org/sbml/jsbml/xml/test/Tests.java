44aa65f4e6aab388d146bb8ac163e42cc891c230
niko-rodrigue <niko.rodrigue@gmail.com>
2015-03-19 16:58:56 +0000

    added the subversion url to the manifest + added the CreatorTests to the test suite.

--- core/test/org/sbml/jsbml/xml/test/Tests.java
+++ core/test/org/sbml/jsbml/xml/test/Tests.java
@@ -40,7 +40,8 @@ import org.sbml.jsbml.test.RemoveFromParentTest;
 @RunWith(value=Suite.class)
 @SuiteClasses(value={SBML_L1VxTests.class, SBML_L2V1Test.class, CheckConsistencyTests.class, GetNotesStringTests.class,
   UnregisterTests.class, RemoveFromParentTest.class, CVTermTests.class, RemoveFromParentTest.class, ASTNodeTest.class,
-  ASTNodeInfixParsingTest.class, TestInfixOperatorPrecedence.class, IdRegistrationTest.class, XMLTokenTest.class})
+  ASTNodeInfixParsingTest.class, TestInfixOperatorPrecedence.class, IdRegistrationTest.class, XMLTokenTest.class,
+  CreatorTests.class})
 public class Tests {
 
 }
