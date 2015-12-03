1c53a41d70da9f039976d2c4c1abf77282e20a02
Andreas Dräger <draeger@informatik.uni-tuebingen.de>
2015-12-03 10:36:57 +0000

    * Improved type hierarchy images of extension packages * Marked ListOfMemberConstraints and MemberConstraint in the goups package as deprecated.

--- extensions/groups/src/org/sbml/jsbml/ext/groups/MemberConstraint.java
+++ extensions/groups/src/org/sbml/jsbml/ext/groups/MemberConstraint.java
@@ -28,14 +28,15 @@ import org.sbml.jsbml.LevelVersionError;
 import org.sbml.jsbml.UniqueNamedSBase;
 
 /**
- * This class was removed from the specifications as of version 0.7 (2015-11-24) as
- * no software wanted to implement support for it.
- * It might be added back in a future version of the specifications if somebody want to implement it. 
- * 
+ * @deprecated This class was removed from the specifications as of version 0.7
+ *             (2015-11-24) as no software wanted to implement support for it.
+ *             It might be added back in a future version of the specifications
+ *             if somebody want to implement it.
  * @author Nicolas Rodriguez
  * @version $Rev$
  * @since 1.0
  */
+@Deprecated
 public class MemberConstraint extends AbstractNamedSBase  implements UniqueNamedSBase {
 
   /**
