8eb11023d30cf9da8d3be4a198d7d24665c56b7a
niko-rodrigue <niko.rodrigue@gmail.com>
2016-01-21 16:34:35 +0000

    implemented AbstractASTNodePlugin in a similar way as AbstractSBasePlugin + added methods on ASTNode to manipulate the plugins, very similar as the ones for SBase

--- core/src/org/sbml/jsbml/AbstractSBase.java
+++ core/src/org/sbml/jsbml/AbstractSBase.java
@@ -1733,14 +1733,6 @@ public abstract class AbstractSBase extends AbstractTreeNode implements SBase {
 
     if (packageParser != null) {
       return extensions.get(packageParser.getPackageName()) != null;
-      // The code below cannot be used as it is, as the package prefix/label can be pass to this method
-      // instead of the namespace URI
-      // TODO - update once the namespace is properly set on plugins
-      //      SBasePlugin extension = extensions.get(packageParser.getPackageName());
-      //      if (extension != null) {
-      //        return extension.getElementNamespace().equals(nameOrUri);
-      //      }
-      //      return false;
     }
 
     throw new IllegalArgumentException(MessageFormat.format(
