bcd87576179db84d314a038e09ee2e24e93f461f
Roman Schulte <roman-schulte@gmx.de>
2016-05-28 21:27:36 +0200

    First constraints (20501 & 20502) added

--- modules/offline-validator/src/org/sbml/jsbml/validator/factory/L3V1FactoryManager.java
+++ modules/offline-validator/src/org/sbml/jsbml/validator/factory/L3V1FactoryManager.java
@@ -1,13 +1,32 @@
 package org.sbml.jsbml.validator.factory;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.sbml.jsbml.Compartment;
+import org.sbml.jsbml.Species;
 
 
 public class L3V1FactoryManager implements FactoryManager {
 
   @Override
   public List<Integer> getIdsForClass(Class<?> clazz, CheckCategory category) {
+    List<Integer> list = new ArrayList<>();
     
-    return null;
+
+	if (clazz.equals(Compartment.class))
+    {
+    	ConstraintFactory.addRangeToList(list, 20507, 20509);
+    	ConstraintFactory.addIntToList(list, 20517);
+    }
+	else if(clazz.equals(Species.class))
+	{
+		ConstraintFactory.addIntToList(list, 20601);
+		ConstraintFactory.addRangeToList(list, 20608, 20611);
+		ConstraintFactory.addIntToList(list, 20614);
+		ConstraintFactory.addIntToList(list, 20617);
+		ConstraintFactory.addIntToList(list, 20623);
+	}
+    return list;
   }
 }
