097855b99ee8be17fba2731989453185ca35d918
Roman Schulte <roman-schulte@gmx.de>
2016-05-29 16:43:16 +0200

    added enum Package

---
+++ core/src/org/sbml/jsbml/Package.java
@@ -0,0 +1,68 @@
+package org.sbml.jsbml;
+
+public enum Package 
+{
+    CORE(0),
+    COMP(1_000_000),
+    FBC(2_000_000),
+    QUAL(3_000_000),
+    LAYOUT(6_000_000);
+
+    
+    public final int offset;
+    
+    private Package(int offset)
+    {
+	this.offset = offset;
+    }
+    
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString().toLowerCase();
+    }
+    
+    
+    public static Package getPackageForError(int errorId)
+    {
+	// Clear the last five digits
+	int offset = (int)((double)(errorId) / 100_000.0) * 100_000;
+	
+	return getPackageWithOffset(offset);
+    }
+    
+    
+    public static Package getPackageWithOffset(int offset)
+    {
+	for (Package p: values())
+	{
+	    if(p.offset == offset)
+	    {
+		return p;
+	    }
+	}
+	
+	return null;
+    }
+    
+    
+    public static Package getPackageWithName(String name)
+    {
+	if (name.isEmpty())
+	{
+	    return CORE;
+	}
+	
+	String n = name.toLowerCase();
+	
+	for (Package p: values())
+	{
+	    if (n.equals(p.toString()))
+	    {
+		return p;
+	    }
+	}
+	
+	return null;
+    }
+}
