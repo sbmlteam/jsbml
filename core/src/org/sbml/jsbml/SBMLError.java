c20ea3c7f016178e844ec3edb6c57dbb0cb31599
Roman Schulte <roman-schulte@gmx.de>
2016-06-22 10:36:06 +0200

    Moved some files

--- core/src/org/sbml/jsbml/SBMLError.java
+++ core/src/org/sbml/jsbml/SBMLError.java
@@ -29,9 +29,6 @@ import java.io.InputStream;
 import java.lang.ref.SoftReference;
 import java.net.URISyntaxException;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.sbml.jsbml.util.Detail;
 import org.sbml.jsbml.util.Message;
 import org.sbml.jsbml.util.ResourceManager;
@@ -81,22 +78,6 @@ public class SBMLError extends XMLException {
      * Generated serial version identifier.
      */
     private static final long serialVersionUID = -6071833473902680630L;
-
-    /**
-     * Keys to access the JSON file
-     */
-    private static final String JSON_KEY_AVAILABLE = "Available";
-    private static final String JSON_KEY_MESSAGE = "Message";
-    private static final String JSON_KEY_SHORT_MESSAGE = "Available";
-    private static final String JSON_KEY_PACKAGE = "Package";
-    private static final String JSON_KEY_CATEGORY = "Category";
-    private static final String JSON_KEY_DEFAULT_SEVERITY = "DefaultSeverity";
-    private static final String JSON_KEY_UNFORMATED_SEVERITY = "SeverityL%dV%d";
-    
-    /**
-     * soft cached json file, which contains the errors
-     */
-    private static SoftReference<JSONObject> cachedJson;
 
     /**
      * 
@@ -439,99 +420,5 @@ public class SBMLError extends XMLException {
      */
     public String getPackage() {
 	return packageName;
-    }
-
-    public static SBMLError loadError(int id, int level, int version) {
-	JSONObject errors = null;
-
-	if (SBMLError.cachedJson != null) {
-	    errors = cachedJson.get();
-	}
-
-	if (errors == null) {
-
-	    try {
-		String fileName = "/org/sbml/jsbml/resources/SBMLErrors.json";
-
-		File file = new File(SBMLError.class.getResource(fileName).toURI());
-
-		JSONParser parser = new JSONParser();
-		errors = (JSONObject) (parser.parse(new FileReader(file)));
-	    }catch (URISyntaxException e)
-	    {
-		
-	    }
-	    catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    } catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    } catch (ParseException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    }
-
-	    SBMLError.cachedJson = new SoftReference<JSONObject>(errors);
-	}
-	
-	if(errors != null && errors.containsKey("" + id))
-	{
-	    JSONObject errorEntry = (JSONObject) errors.get("" + id);
-	    
-	    
-	    if(errorEntry != null && isAvailable(errorEntry, level, version))
-	    {
-		SBMLError e = new SBMLError();
-		
-		e.setCode(id);
-		e.setCategory((String)errorEntry.get(JSON_KEY_CATEGORY));
-		
-		Message m = new Message();
-		m.setMessage((String)errorEntry.get(JSON_KEY_MESSAGE));
-		e.setMessage(m);
-		
-		Message sm = new Message();
-		sm.setMessage((String) errorEntry.get(JSON_KEY_SHORT_MESSAGE));
-		
-		e.setShortMessage(sm);
-		
-		e.setPackage((String)errorEntry.get(JSON_KEY_PACKAGE));
-		
-		
-//		System.out.println("Out: " + e + "   " + e.getShortMessage());
-		return e;
-	    }
-	    
-	}
-	
-	return null;
-    }
-    
-    private static boolean isAvailable(JSONObject error, int level, int version)
-    {
-	String minLv = (String)(error.get(JSON_KEY_AVAILABLE));
-	
-	if(minLv != null)
-	{
-	    String[] blocks = minLv.substring(1).split("V");
-	    
-	    if(blocks.length == 2)
-	    {
-		int l = Integer.parseInt(blocks[0]);
-		int v = Integer.parseInt(blocks[1]);
-		
-		// Return true if level is greater as the minimal level
-		// Or if the level is equal, but the version greater equal as the min verison.
-		return (level > l) || (level == l && version >= v);
-	    }
-	}
-	
-	return true;
-    }
-    
-    private static String getSeverityKey(int level, int version)
-    {
-	return String.format(JSON_KEY_UNFORMATED_SEVERITY, level, version);
     }
 }
