6da873f27b2cb4ea49285f9908d185baad166166
Roman Schulte <roman-schulte@gmx.de>
2016-06-28 17:20:49 +0200

    Removed FactoryManager

--- core/src/org/sbml/jsbml/validator/offline/constraints/CoreConstraintBuilder.java
+++ core/src/org/sbml/jsbml/validator/offline/constraints/CoreConstraintBuilder.java
@@ -1,95 +1,129 @@
 package org.sbml.jsbml.validator.offline.constraints;
 
 import org.sbml.jsbml.Compartment;
+import org.sbml.jsbml.CompartmentType;
+import org.sbml.jsbml.Constraint;
+import org.sbml.jsbml.Event;
+import org.sbml.jsbml.FunctionDefinition;
 import org.sbml.jsbml.InitialAssignment;
 import org.sbml.jsbml.Model;
+import org.sbml.jsbml.Parameter;
+import org.sbml.jsbml.Reaction;
+import org.sbml.jsbml.Rule;
+import org.sbml.jsbml.SBMLDocument;
 import org.sbml.jsbml.Species;
 import org.sbml.jsbml.SpeciesReference;
+import org.sbml.jsbml.SpeciesType;
 import org.sbml.jsbml.UnitDefinition;
+import org.sbml.jsbml.util.TreeNodeChangeListener;
 import org.sbml.jsbml.validator.offline.ValidationContext;
+import org.sbml.jsbml.validator.offline.factory.ConstraintFactory;
 
+@SuppressWarnings("deprecation")
 public class CoreConstraintBuilder extends AbstractConstraintBuilder {
 
     @Override
     public AnyConstraint<?> createConstraint(int id) {
-	// Using bisection to find constraints faster.
-	// using comments to mark the intervalls
 
-	// id -> (50_000, 99_999]
-	if (id > 50_000) {
-	    // id -> (75_000, 99_999]
-	    if (id > 75_000) {
+	// int uid = Math.abs(id);
 
-	    }
-	    // id -> (50_000, 75_000]
-	    else {
-
-	    }
-	}
-	// id -> (0, 50_000]
-	else {
-	    // id -> (25_000, 50_000]
-	    if (id > 25_000) {
-
-	    }
-	    // id -> (0, 25_000]
-	    else {
-
-		// id -> (12_500, 25_000]
-		if (id > 12_500) {
-		    // id -> (18_000, 25_000]
-		    if (id > 18_000) {
-			// id -> (20_000, 25_000]
-			if (id > 20_000) {
-			    // id -> (22_000, 25_000]
-			    if (id > 22_000) {
-
-			    }
-			    // id -> (20_000, 22_000]
-			    else {
-				// id -> (21_000, 22_000]
-				if (id > 21_000) {
-
-				}
-				// id -> (20_000, 21_000]
-				else {
-				    if (id > 20_700) {
-					return createInitialAssignment(id);
-				    } else if (id > 20_600) {
-					return createSpeciesConstraint(id);
-				    } else if (id > 20_500) {
-					return createCompartmentConstraint(id);
-				    }
-				}
-			    }
-			}
-			// id -> (18_000, 20_000]
-			else {
-			    // id -> (19_000, 20_000]
-			    if (id > 19_000) {
-
-			    }
-			    // id -> (18_000, 19_000]
-			    else {
-
-			    }
-			}
-		    } else {
-
-		    }
-		    if (id > 20_000) {
-
-		    } else {
-
-		    }
-		    // id -> (0, 12_500]
-		} else {
-
-		}
-	    }
+	if (id < 0) {
+	    return this.createSpecialConstraint(id);
 	}
 
-	return null;
+	int firstThree = id / 100;
+
+	switch (firstThree) {
+
+	case 207:
+	    return CoreConstraintBuilder.createInitialAssignmentConstraint(id);
+	case 206:
+	    return CoreConstraintBuilder.createSpeciesConstraint(id);
+	case 205:
+	    return CoreConstraintBuilder.createCompartmentConstraint(id);
+
+	default:
+	    return null;
+	}
+
+	// // Using bisection to find constraints faster.
+	// // using comments to mark the intervalls
+	//
+	// // id -> (50_000, 99_999]
+	// if (id > 50_000) {
+	// // id -> (75_000, 99_999]
+	// if (id > 75_000) {
+	//
+	// }
+	// // id -> (50_000, 75_000]
+	// else {
+	//
+	// }
+	// }
+	// // id -> (0, 50_000]
+	// else {
+	// // id -> (25_000, 50_000]
+	// if (id > 25_000) {
+	//
+	// }
+	// // id -> (0, 25_000]
+	// else {
+	//
+	// // id -> (12_500, 25_000]
+	// if (id > 12_500) {
+	// // id -> (18_000, 25_000]
+	// if (id > 18_000) {
+	// // id -> (20_000, 25_000]
+	// if (id > 20_000) {
+	// // id -> (22_000, 25_000]
+	// if (id > 22_000) {
+	//
+	// }
+	// // id -> (20_000, 22_000]
+	// else {
+	// // id -> (21_000, 22_000]
+	// if (id > 21_000) {
+	//
+	// }
+	// // id -> (20_000, 21_000]
+	// else {
+	// if (id > 20_700) {
+	// return createInitialAssignment(id);
+	// } else if (id > 20_600) {
+	// return createSpeciesConstraint(id);
+	// } else if (id > 20_500) {
+	// return createCompartmentConstraint(id);
+	// }
+	// }
+	// }
+	// }
+	// // id -> (18_000, 20_000]
+	// else {
+	// // id -> (19_000, 20_000]
+	// if (id > 19_000) {
+	//
+	// }
+	// // id -> (18_000, 19_000]
+	// else {
+	//
+	// }
+	// }
+	// } else {
+	//
+	// }
+	// if (id > 20_000) {
+	//
+	// } else {
+	//
+	// }
+	// // id -> (0, 12_500]
+	// } else {
+	//
+	// }
+	// }
+	// }
+
+	// return null;
     }
 
     /**
@@ -230,7 +264,7 @@ public class CoreConstraintBuilder extends AbstractConstraintBuilder {
 
 	case CORE_20510:
 	    func = new ValidationFunction<Compartment>() {
-		@SuppressWarnings("deprecation")
+//		@SuppressWarnings("deprecation")
 		@Override
 		public boolean check(ValidationContext ctx, Compartment c) {
 
@@ -498,7 +532,7 @@ public class CoreConstraintBuilder extends AbstractConstraintBuilder {
 	return new ValidationConstraint<Species>(id, func);
     }
 
-    protected static ValidationConstraint<?> createInitialAssignment(int id) {
+    protected static ValidationConstraint<?> createInitialAssignmentConstraint(int id) {
 	ValidationFunction<InitialAssignment> f = null;
 
 	switch (id) {
@@ -558,6 +592,175 @@ public class CoreConstraintBuilder extends AbstractConstraintBuilder {
 	}
 
 	return new ValidationConstraint<InitialAssignment>(id, f);
+    }
+
+    private AnyConstraint<?> createSpecialConstraint(int id) {
+	switch (id) {
+	case CoreSpecialErrorCodes.ID_EMPTY_CONSTRAINT:
+	    return new ValidationConstraint<>(id, null);
+
+	case CoreSpecialErrorCodes.ID_VALIDATE_DOCUMENT_TREE:
+	    ValidationFunction<SBMLDocument> f2 = new ValidationFunction<SBMLDocument>() {
+		@Override
+		public boolean check(ValidationContext ctx, SBMLDocument t) {
+		    /*
+		     * Special constraint to validate the hole document tree
+		     */
+		    System.out.println("validate doc tree");
+		    ConstraintFactory factory = ConstraintFactory.getInstance();
+
+		    AnyConstraint<Model> mc = factory.getConstraintsForClass(Model.class, ctx.getCheckCategories(),
+			    ctx.getPackages(), ctx.getLevel(), ctx.getVersion());
+		    mc.check(ctx, t.getModel());
+
+		    return true;
+		}
+	    };
+
+	    return new ValidationConstraint<SBMLDocument>(id, f2);
+
+	case CoreSpecialErrorCodes.ID_VALIDATE_MODEL_TREE:
+	    ValidationFunction<Model> f3 = new ValidationFunction<Model>() {
+		@SuppressWarnings("deprecation")
+		@Override
+		public boolean check(ValidationContext ctx, Model t) {
+		    /*
+		     * Special constraint to validate the hole model tree
+		     */
+		    ConstraintFactory factory = ConstraintFactory.getInstance();
+
+		    {
+			AnyConstraint<Species> sc = factory.getConstraintsForClass(Species.class,
+				ctx.getCheckCategories(), ctx.getPackages(), ctx.getLevel(), ctx.getVersion());
+
+			for (Species s : t.getListOfSpecies()) {
+			    sc.check(ctx, s);
+			}
+		    }
+
+		    {
+
+			AnyConstraint<Compartment> cc = factory.getConstraintsForClass(Compartment.class,
+				ctx.getCheckCategories(), ctx.getPackages(), ctx.getLevel(), ctx.getVersion());
+
+			for (Compartment c : t.getListOfCompartments()) {
+			    cc.check(ctx, c);
+			}
+		    }
+
+		    {
+
+			AnyConstraint<CompartmentType> cc = factory.getConstraintsForClass(CompartmentType.class,
+				ctx.getCheckCategories(), ctx.getPackages(), ctx.getLevel(), ctx.getVersion());
+
+			for (CompartmentType c : t.getListOfCompartmentTypes()) {
+			    cc.check(ctx, c);
+			}
+		    }
+
+		    {
+			AnyConstraint<Constraint> cc = factory.getConstraintsForClass(Constraint.class,
+				ctx.getCheckCategories(), ctx.getPackages(), ctx.getLevel(), ctx.getVersion());
+
+			for (Constraint c : t.getListOfConstraints()) {
+			    cc.check(ctx, c);
+			}
+		    }
+
+		    {
+			AnyConstraint<Event> cc = factory.getConstraintsForClass(Event.class, ctx.getCheckCategories(),
+				ctx.getPackages(), ctx.getLevel(), ctx.getVersion());
+
+			for (Event c : t.getListOfEvents()) {
+			    cc.check(ctx, c);
+			}
+		    }
+
+		    {
+			AnyConstraint<FunctionDefinition> cc = factory.getConstraintsForClass(FunctionDefinition.class,
+				ctx.getCheckCategories(), ctx.getPackages(), ctx.getLevel(), ctx.getVersion());
+
+			for (FunctionDefinition c : t.getListOfFunctionDefinitions()) {
+			    cc.check(ctx, c);
+			}
+		    }
+
+		    {
+			AnyConstraint<InitialAssignment> cc = factory.getConstraintsForClass(InitialAssignment.class,
+				ctx.getCheckCategories(), ctx.getPackages(), ctx.getLevel(), ctx.getVersion());
+
+			for (InitialAssignment c : t.getListOfInitialAssignments()) {
+			    cc.check(ctx, c);
+			}
+		    }
+
+		    {
+			AnyConstraint<Parameter> cc = factory.getConstraintsForClass(Parameter.class,
+				ctx.getCheckCategories(), ctx.getPackages(), ctx.getLevel(), ctx.getVersion());
+
+			for (Parameter c : t.getListOfParameters()) {
+			    cc.check(ctx, c);
+			}
+		    }
+
+		    {
+			AnyConstraint<UnitDefinition> cc = factory.getConstraintsForClass(UnitDefinition.class,
+				ctx.getCheckCategories(), ctx.getPackages(), ctx.getLevel(), ctx.getVersion());
+
+			for (UnitDefinition c : t.getListOfPredefinedUnitDefinitions()) {
+			    cc.check(ctx, c);
+			}
+
+			for (UnitDefinition ud : t.getListOfUnitDefinitions()) {
+			    cc.check(ctx, ud);
+			}
+		    }
+
+		    {
+			AnyConstraint<Reaction> cc = factory.getConstraintsForClass(Reaction.class,
+				ctx.getCheckCategories(), ctx.getPackages(), ctx.getLevel(), ctx.getVersion());
+
+			for (Reaction c : t.getListOfReactions()) {
+			    cc.check(ctx, c);
+			}
+		    }
+
+		    {
+			AnyConstraint<Rule> cc = factory.getConstraintsForClass(Rule.class, ctx.getCheckCategories(),
+				ctx.getPackages(), ctx.getLevel(), ctx.getVersion());
+
+			for (Rule c : t.getListOfRules()) {
+			    cc.check(ctx, c);
+			}
+		    }
+
+		    {
+
+			AnyConstraint<SpeciesType> cc = factory.getConstraintsForClass(SpeciesType.class,
+				ctx.getCheckCategories(), ctx.getPackages(), ctx.getLevel(), ctx.getVersion());
+
+			for (SpeciesType c : t.getListOfSpeciesTypes()) {
+			    cc.check(ctx, c);
+			}
+		    }
+
+		    {
+			AnyConstraint<TreeNodeChangeListener> cc = factory.getConstraintsForClass(
+				TreeNodeChangeListener.class, ctx.getCheckCategories(), ctx.getPackages(), ctx.getLevel(), ctx.getVersion());
+
+			for (TreeNodeChangeListener c : t.getListOfTreeNodeChangeListeners()) {
+			    cc.check(ctx, c);
+			}
+		    }
+
+		    return true;
+		}
+	    };
+
+	    return new ValidationConstraint<Model>(id, f3);
+	default:
+	    return null;
+	}
     }
 
 }
