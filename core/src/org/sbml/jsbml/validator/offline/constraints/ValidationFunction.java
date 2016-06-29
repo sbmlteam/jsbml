b46f2a885f09bc4b63e6c10315107b4cfd05f8d2
Roman Schulte <roman-schulte@gmx.de>
2016-06-29 21:40:48 +0200

    Adjusted code style

--- core/src/org/sbml/jsbml/validator/offline/constraints/ValidationFunction.java
+++ core/src/org/sbml/jsbml/validator/offline/constraints/ValidationFunction.java
@@ -2,7 +2,7 @@ package org.sbml.jsbml.validator.offline.constraints;
 
 import org.sbml.jsbml.validator.offline.ValidationContext;
 
-public interface ValidationFunction<T>
-{
+public interface ValidationFunction<T> {
+
   public boolean check(ValidationContext ctx, T t);
 }
