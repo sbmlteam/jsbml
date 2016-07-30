263613eafcc6fbf35864fc5750b459cd8f1ab985
Roman Schulte <roman-schulte@gmx.de>
2016-07-30 11:27:41 +0200

    Add new constraints (21204-21231)

--- core/src/org/sbml/jsbml/validator/offline/constraints/CoreConstraintBuilder.java
+++ core/src/org/sbml/jsbml/validator/offline/constraints/CoreConstraintBuilder.java
@@ -30,16 +30,21 @@ import javax.swing.tree.TreeNode;
 import org.sbml.jsbml.ASTNode;
 import org.sbml.jsbml.Compartment;
 import org.sbml.jsbml.Constraint;
+import org.sbml.jsbml.Delay;
+import org.sbml.jsbml.Event;
+import org.sbml.jsbml.EventAssignment;
 import org.sbml.jsbml.ExplicitRule;
 import org.sbml.jsbml.FunctionDefinition;
 import org.sbml.jsbml.InitialAssignment;
 import org.sbml.jsbml.KineticLaw;
 import org.sbml.jsbml.Model;
 import org.sbml.jsbml.Parameter;
+import org.sbml.jsbml.Priority;
 import org.sbml.jsbml.Reaction;
 import org.sbml.jsbml.SimpleSpeciesReference;
 import org.sbml.jsbml.Species;
 import org.sbml.jsbml.SpeciesReference;
+import org.sbml.jsbml.Trigger;
 import org.sbml.jsbml.Unit;
 import org.sbml.jsbml.UnitDefinition;
 import org.sbml.jsbml.Variable;
@@ -62,6 +67,8 @@ public class CoreConstraintBuilder extends AbstractConstraintBuilder {
 
     switch (firstThree) 
     {
+    case 211:
+      return CoreConstraintBuilder.createEventConstraint(errorCode);
     case 210:
       return CoreConstraintBuilder.createConstraintConstraint(errorCode);
     case 209:
@@ -150,19 +157,34 @@ public class CoreConstraintBuilder extends AbstractConstraintBuilder {
             if (ctx.isLevelAndVersionLessThan(2, 3))
             {
               // must be a lambda
-              if (!math.isLambda())
+              if (!math.isLambda() || math.isSemantics())
               {
                 success = false;
               }
 
-              // TODO isSemantics???
             }
             else
             {
-              // Only applies to lambdas in l2v3 and later.
-              if (math.isLambda())
+              // Only applies to non lambdas in l2v3 and later.
+              if (!math.isLambda())
               {
-                //TODO isSemantics???
+                if (math.isSemantics())
+                {
+                  if (math.getNumChildren() == 1)
+                  {
+                    if(!math.getChild(0).isLambda())
+                    {
+                      success = false;
+                    }
+                  }
+                  else
+                  {
+                    success = false;
+                  }
+                }
+                else{
+                  success = false;
+                }
               }
             }
           } 
@@ -1188,7 +1210,7 @@ public class CoreConstraintBuilder extends AbstractConstraintBuilder {
           return !kl.isSetSubstanceUnits();
         }
       });
-      
+
     case CORE_21126:
       return new ValidationConstraint<KineticLaw>(errorCode, new ValidationFunction<KineticLaw>() {
         @Override
@@ -1197,7 +1219,7 @@ public class CoreConstraintBuilder extends AbstractConstraintBuilder {
           return !kl.isSetTimeUnits();
         }
       });
-      
+
     case CORE_21130:
       return new ValidationConstraint<KineticLaw>(errorCode, new ValidationFunction<KineticLaw>() {
         @Override
@@ -1210,6 +1232,194 @@ public class CoreConstraintBuilder extends AbstractConstraintBuilder {
     return new ValidationConstraint<Reaction>(errorCode, func);
   }
 
+  protected static AnyConstraint<?> createEventConstraint(int errorCode) {
+    ValidationFunction<Event> func = null;
+
+    switch (errorCode) {
+    case CORE_21201:
+
+      func = new ValidationFunction<Event>() {
+
+
+        @Override
+        public boolean check(ValidationContext ctx, Event e) {
+
+          return e.isSetTrigger();
+        }
+      };
+
+    case CORE_21202:
+      return new ValidationConstraint<Trigger>(errorCode, new ValidationFunction<Trigger>() {
+        @Override
+        public boolean check(ValidationContext ctx, Trigger t) {
+
+          if (t.isSetMath())
+          {
+            return t.getMath().isBoolean();
+          }
+
+          return false;
+        }
+      });
+
+    case CORE_21203:
+
+      func = new ValidationFunction<Event>() {
+
+        @Override
+        public boolean check(ValidationContext ctx, Event e) {
+
+          return e.getNumEventAssignments() != 0;
+        }
+      };
+
+    case CORE_21204:
+
+      func = new ValidationFunction<Event>() {
+
+        @Override
+        public boolean check(ValidationContext ctx, Event e) {
+
+          if (e.isSetTimeUnits())
+          {
+            String s = e.getTimeUnits();
+            UnitDefinition def = e.getTimeUnitsInstance();
+
+
+            boolean isTime = (s == "time") || 
+                (s == "second") || 
+                (def.isVariantOfTime());
+
+            if (ctx.isLevelAndVersionEqualTo(2, 2))
+            {
+              return isTime;
+            }
+            else
+            {
+              return isTime ||
+                  (s == "dimensionless");
+            }
+          }
+
+          return e.getNumEventAssignments() != 0;
+        }
+      };
+
+    case CORE_21206:
+
+      func = new ValidationFunction<Event>() {
+
+        @Override
+        public boolean check(ValidationContext ctx, Event e) {
+
+          if (!e.getUseValuesFromTriggerTime()){
+            return e.isSetDelay();
+          }
+
+          return true;
+        }
+      };
+
+    case CORE_21207:
+
+      func = new ValidationFunction<Event>() {
+
+        @Override
+        public boolean check(ValidationContext ctx, Event e) {
+
+          if (e.isSetDelay())
+          {
+            return e.isSetUseValuesFromTriggerTime();
+          }
+
+          return true;
+        }
+      };
+
+    case CORE_21209:
+      return new ValidationConstraint<Trigger>(errorCode, new ValidationFunction<Trigger>() {
+        @Override
+        public boolean check(ValidationContext ctx, Trigger t) {
+
+
+          return t.isSetMath();
+        }
+      });
+
+    case CORE_21210:
+      return new ValidationConstraint<Delay>(errorCode, new ValidationFunction<Delay>() {
+        @Override
+        public boolean check(ValidationContext ctx, Delay d) {
+
+
+          return d.isSetMath();
+        }
+      });
+
+    case CORE_21211:
+      return new ValidationConstraint<EventAssignment>(errorCode, new ValidationFunction<EventAssignment>() {
+        @Override
+        public boolean check(ValidationContext ctx, EventAssignment ea) {
+
+          if (ea.isSetVariable())
+          {
+            Variable var = ea.getVariableInstance();
+            boolean isComSpecOrPar =  (var instanceof Compartment) ||
+                (var instanceof Species) ||
+                (var instanceof Parameter);
+
+            if (ctx.getLevel() == 2){
+              return isComSpecOrPar;
+            }
+            else{
+              return isComSpecOrPar || (var instanceof SpeciesReference);
+            }
+
+          } 
+
+          return true;
+        }
+      });
+      
+    case CORE_21212:
+      return new ValidationConstraint<EventAssignment>(errorCode, new ValidationFunction<EventAssignment>() {
+        @Override
+        public boolean check(ValidationContext ctx, EventAssignment ea) {
+
+          if(ea.isSetVariable())
+          {
+            Variable var = ea.getVariableInstance();
+            
+            return var != null && var.isConstant();
+          }
+          
+          return true;
+        }
+      });
+      
+    case CORE_21213:
+      return new ValidationConstraint<EventAssignment>(errorCode, new ValidationFunction<EventAssignment>() {
+        @Override
+        public boolean check(ValidationContext ctx, EventAssignment ea) {
+
+         return ea.isSetMath();
+        }
+      });
+      
+    case CORE_21231:
+      return new ValidationConstraint<Priority>(errorCode, new ValidationFunction<Priority>() {
+        @Override
+        public boolean check(ValidationContext ctx, Priority p) {
+
+         return p.isSetMath();
+        }
+      });
+    }
+
+
+
+    return new ValidationConstraint<Event>(errorCode, func);
+  }
 
   protected static AnyConstraint<?> createSpecialConstraint(int errorCode) {
     switch (errorCode) {
