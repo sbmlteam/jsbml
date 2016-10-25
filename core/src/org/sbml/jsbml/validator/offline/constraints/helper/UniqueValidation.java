1ee3a469e251e4e32eeac7b93f1f7cbe6796cd94
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2016-10-25 13:56:30 +0100

    fixed constraint 10301 by excluding empty Strings from the java set.

--- core/src/org/sbml/jsbml/validator/offline/constraints/helper/UniqueValidation.java
+++ core/src/org/sbml/jsbml/validator/offline/constraints/helper/UniqueValidation.java
@@ -1,3 +1,22 @@
+/*
+ * 
+ * ----------------------------------------------------------------------------
+ * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
+ * for the latest version of JSBML and more information about SBML.
+ * Copyright (C) 2009-2016 jointly by the following organizations:
+ * 1. The University of Tuebingen, Germany
+ * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ * 3. The California Institute of Technology, Pasadena, CA, USA
+ * 4. The University of California, San Diego, La Jolla, CA, USA
+ * 5. The Babraham Institute, Cambridge, UK
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation. A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution
+ * and also available online as <http://sbml.org/Software/JSBML/License>.
+ * ----------------------------------------------------------------------------
+ */
+
 package org.sbml.jsbml.validator.offline.constraints.helper;
 
 import java.util.HashSet;
@@ -11,7 +30,6 @@ import org.sbml.jsbml.validator.offline.constraints.ValidationFunction;
  * 
  * @author Roman
  * @since 1.2
- * @date 05.08.2016
  */
 public abstract class UniqueValidation<T, U> implements ValidationFunction<T>{
 
@@ -28,6 +46,14 @@ public abstract class UniqueValidation<T, U> implements ValidationFunction<T>{
       // If id not null, but already in set
       if (id != null && !ids.add(id))
       {
+        if (id instanceof String) {
+          String idStr = (String) id;
+          
+          if (idStr.trim().length() == 0) {
+            continue;
+          }
+        }
+        
         ids.clear();
         return false; // TODO - how do we provides the id and the elements that had the duplicated id ?
       }
