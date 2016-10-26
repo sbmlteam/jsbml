178046dfc8c10e453788529d08dc1bbfb1fefc47
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2016-10-26 16:13:04 +0100

    change the version back to 1.2-SNAPSHOT everywhere

--- core/src/org/sbml/jsbml/JSBML.java
+++ core/src/org/sbml/jsbml/JSBML.java
@@ -77,7 +77,7 @@ public class JSBML {
   /**
    * The current version number of JSBML.
    */
-  private static final String jsbmlVersion = "1.2-beta1"; // TODO: replace automatically this version number with [BUILD.NUMBER]
+  private static final String jsbmlVersion = "1.2-SNAPSHOT"; // TODO: replace automatically this version number with [BUILD.NUMBER]
   /**
    * 
    */
