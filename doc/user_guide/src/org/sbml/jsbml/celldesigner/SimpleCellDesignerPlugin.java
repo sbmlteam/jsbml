407b91b9249a227d52300b031812b1661e6bbf7a
Michael Hucka <mhucka@caltech.edu>
2016-12-07 17:05:46 -0800

    Really tightened up vertical space so it fits one page.

--- doc/user_guide/src/org/sbml/jsbml/celldesigner/SimpleCellDesignerPlugin.java
+++ doc/user_guide/src/org/sbml/jsbml/celldesigner/SimpleCellDesignerPlugin.java
@@ -32,95 +32,64 @@ import jp.sbi.celldesigner.plugin.PluginSBase;
 
 import org.sbml.jsbml.gui.JSBMLvisualizer;
 
-/**
- * A simple plugin for CellDesigner that displays the SBML data structure as a
- * tree. When the underlying SBMLDocument is changed by CellDesigner,
- * the tree will refresh itself and all nodes will become unexpanded.
- */
+/** Simple CellDesigner plugin to display the SBML data structure as a tree. When the underlying SBMLDocument
+ *  is changed by CellDesigner, the tree will refresh itself and all nodes will become unexpanded. */
 public class SimpleCellDesignerPlugin extends AbstractCellDesignerPlugin {
-
-  /**
-   * 
-   */
   public static final String ACTION = "Display full model tree";
-  /**
-   * 
-   */
   public static final String APPLICATION_NAME = "Simple Plugin";
-  /**
-   * 
-   */
   protected DefaultTreeModel modelTree = null;
 
-  /**
-   * Creates a new CellDesigner plug-in with an entry in the menu bar.
-   */
   public SimpleCellDesignerPlugin() {
     super();
     addPluginMenu();
   }
 
   @Override
-  public void addPluginMenu() {
-    // Initializing CellDesigner's menu entries
+  public void addPluginMenu() {    // Initializing CellDesigner's menu entries
     PluginMenu menu = new PluginMenu(APPLICATION_NAME);
-    PluginMenuItem menuItem = new PluginMenuItem(
-      ACTION, new SimpleCellDesignerPluginAction(this));
+    PluginMenuItem menuItem = new PluginMenuItem(ACTION, new SimpleCellDesignerPluginAction(this));
     menuItem.setToolTipText("Displays the data structure of the model.");
     menu.add(menuItem);
     addCellDesignerPluginMenu(menu);
   }
 
-  /**
-   * After the model is changed, refreshes the Tree by resetting the Root node.
-   */
+  // After the model is changed, refreshes the Tree by resetting the Root node.
   @Override
   public void modelSelectChanged(PluginSBase sbase) {
     super.modelSelectChanged(sbase);
     modelTree.setRoot(getSBMLDocument());
   }
 
-  /**
-   * After a PluginSBase addition, refreshes the Tree by resetting the Root node.
-   */
+  // After a PluginSBase addition, refreshes the Tree by resetting the Root node.
   @Override
   public void SBaseAdded(PluginSBase sbase) {
     super.SBaseAdded(sbase);
     modelTree.setRoot(getSBMLDocument());
   }
 
-  /**
-   * After a PluginSBase modification, refreshes the Tree by resetting the Root node.
-   */
+  // After a PluginSBase modification, refreshes the Tree by resetting the Root node.
   @Override
   public void SBaseChanged(PluginSBase sbase) {
     super.SBaseChanged(sbase);
     modelTree.setRoot(getSBMLDocument());
   }
 
-  /**
-   * After a PluginSBase deletion, refreshes the Tree by resetting the Root node.
-   */
+  // After a PluginSBase deletion, refreshes the Tree by resetting the Root node.
   @Override
   public void SBaseDeleted(PluginSBase sbase) {
     super.SBaseDeleted(sbase);
     modelTree.setRoot(getSBMLDocument());
   }
 
-  /**
-   * If the CellDesigner model is closed, we nullify the Tree.
-   */
+  // If the CellDesigner model is closed, we nullify the Tree.
   @Override
   public void modelClosed(PluginSBase sbase) {
     super.modelClosed(sbase);
     modelTree.setRoot(null);
   }
 
-  /**
-   * Initializes plugin and sets WindowClosed() events.
-   */
   @Override
-  public void run() {
+  public void run() {  // Initializes plugin and sets WindowClosed() events.
     JSBMLvisualizer visualizer = new JSBMLvisualizer(getSBMLDocument());
     visualizer.addWindowListener(new WindowAdapter() {
       @Override
