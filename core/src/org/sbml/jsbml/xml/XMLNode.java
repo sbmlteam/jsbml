29ac91e2ae98895b1637a096d59a46f9b9226b43
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2016-12-09 15:14:24 +0000

    modified the default toString() to print the size of ListOf or other collections/arrays and for other instances of TreeNode, it just say that is is set

--- core/src/org/sbml/jsbml/xml/XMLNode.java
+++ core/src/org/sbml/jsbml/xml/XMLNode.java
@@ -1,6 +1,5 @@
 /*
- * $Id$
- * $URL$
+ * 
  * ----------------------------------------------------------------------------
  * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
  * for the latest version of JSBML and more information about SBML.
@@ -101,7 +100,6 @@ import org.sbml.jsbml.xml.stax.SBMLReader;
  * 
  * @author Nicolas Rodriguez
  * @since 0.8
- * @version $Rev$
  */
 public class XMLNode extends XMLToken {
 
