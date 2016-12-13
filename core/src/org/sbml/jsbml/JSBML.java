9dd22925ecfbed9032c11265411c58bb8200a372
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2016-12-13 11:05:21 +0000

    updated the version number to 1.2

--- core/src/org/sbml/jsbml/JSBML.java
+++ core/src/org/sbml/jsbml/JSBML.java
@@ -77,7 +77,7 @@ public class JSBML {
   /**
    * The current version number of JSBML.
    */
-  private static final String jsbmlVersion = "1.2-SNAPSHOT"; // TODO: replace automatically this version number with [BUILD.NUMBER]
+  private static final String jsbmlVersion = "1.2"; // TODO: replace automatically this version number with [BUILD.NUMBER]
   /**
    * 
    */
