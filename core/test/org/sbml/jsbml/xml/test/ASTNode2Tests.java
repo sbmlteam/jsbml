edea49037918d9648e0e9bad0505d970cf251bed
draeger <andreas.draeger@uni-tuebingen.de>
2017-01-11 17:30:45 +0100

    Updated all license headers to 2017.

--- core/test/org/sbml/jsbml/xml/test/ASTNode2Tests.java
+++ core/test/org/sbml/jsbml/xml/test/ASTNode2Tests.java
@@ -1,17 +1,15 @@
 /*
- * $Id$
- * $URL$
  * ---------------------------------------------------------------------------- 
  * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML> 
  * for the latest version of JSBML and more information about SBML. 
  * 
- * Copyright (C) 2009-2014  jointly by the following organizations: 
+ * Copyright (C) 2009-2017 jointly by the following organizations: 
  * 1. The University of Tuebingen, Germany 
  * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK 
  * 3. The California Institute of Technology, Pasadena, CA, USA 
  * 4. The University of California, San Diego, La Jolla, CA, USA
  * 5. The Babraham Institute, Cambridge, UK
- * 6. The University of Toronto, Toronto, Ontario, Canada
+ * 6. The University of Toronto, Toronto, ON, Canada
  * 
  * This library is free software; you can redistribute it and/or modify it 
  * under the terms of the GNU Lesser General Public License as published by 
