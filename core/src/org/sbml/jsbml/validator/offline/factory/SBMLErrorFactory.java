b865e130d9fb7a4adbcc3bfb8361602d198638cf
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2017-06-08 13:29:47 +0100

    fixed the json keys, so that we can load the error short message + added documentation.

--- core/src/org/sbml/jsbml/validator/offline/factory/SBMLErrorFactory.java
+++ core/src/org/sbml/jsbml/validator/offline/factory/SBMLErrorFactory.java
@@ -29,66 +29,110 @@ import org.json.simple.parser.JSONParser;
 import org.sbml.jsbml.SBMLError;
 import org.sbml.jsbml.util.Message;
 
+/**
+ * Creates {@link SBMLError} populated with values coming from a json file.
+ * 
+ * <p>This json file is generated from the libSBML source tree.</p>
+ * 
+ * @author Roman
+ * @author rodrigue
+ * @since 1.2
+ */
 public class SBMLErrorFactory {
 
   /**
-   * Keys to access the JSON file
+   * Key for the 'Available' value in the JSON file
    */
-  private static final String              JSON_KEY_AVAILABLE           =
-    "Available";
-  private static final String              JSON_KEY_MESSAGE             =
-    "Message";
-  private static final String              JSON_KEY_SHORT_MESSAGE       =
-    "Available";
-  private static final String              JSON_KEY_PACKAGE             =
-    "Package";
-  private static final String              JSON_KEY_CATEGORY            =
-    "Category";
-  private static final String              JSON_KEY_DEFAULT_SEVERITY    =
-    "DefaultSeverity";
-  private static final String              JSON_KEY_UNFORMATED_SEVERITY =
-    "SeverityL%dV%d";
+  private static final String              JSON_KEY_AVAILABLE           = "Available";
+  /**
+   * Key for the 'Message' value in the JSON file
+   */
+  private static final String              JSON_KEY_MESSAGE             = "Message";
+  /**
+   * Key for the 'ShortMessage' value in the JSON file
+   */
+  private static final String              JSON_KEY_SHORT_MESSAGE       = "ShortMessage";
+  /**
+   * Key for the 'Package' value in the JSON file
+   */
+  private static final String              JSON_KEY_PACKAGE             = "Package";
+  /**
+   * Key for the 'Category' value in the JSON file
+   */
+  private static final String              JSON_KEY_CATEGORY            = "Category";
+  /**
+   * Key for the 'DefaultSeverity' value in the JSON file
+   */
+  private static final String              JSON_KEY_DEFAULT_SEVERITY    = "DefaultSeverity";
+  /**
+   * Key for the 'Severity' value for a certain SBML level and version in the JSON file
+   */
+  private static final String              JSON_KEY_UNFORMATED_SEVERITY = "SeverityL%dV%d";
+  
+  /**
+   * the cache for the json object to avoid to read the json file too often.
+   */
   private static SoftReference<JSONObject> cachedJson;
 
 
+  /**
+   * Creates a new {@link SBMLError} instance. 
+   * 
+   * @param id the error id
+   * @param level the SBML level
+   * @param version the SBML version
+   * @return a new {@link SBMLError} instance. 
+   */
   public static SBMLError createError(int id, int level, int version) {
     JSONObject errors = null;
+    
+    // Trying to get the json object from the cache
     if (SBMLErrorFactory.cachedJson != null) {
       errors = cachedJson.get();
     }
+    
+    // if the json object is null, populate it from the json file and put it on the cache.
     if (errors == null) {
       try {
         String fileName = "../../../resources/SBMLErrors.json";
-        // JSONParser parser2 = new JSONParser();
-        File file =
-          new File(SBMLErrorFactory.class.getResource(fileName).getFile());
+
+        File file = new File(SBMLErrorFactory.class.getResource(fileName).getFile());
         JSONParser parser = new JSONParser();
+        
         errors = (JSONObject) (parser.parse(new FileReader(file)));
       } catch (Exception e) {
         e.printStackTrace();
       }
       SBMLErrorFactory.cachedJson = new SoftReference<JSONObject>(errors);
     }
+    
     if (errors != null && errors.containsKey("" + id)) {
       JSONObject errorEntry = (JSONObject) errors.get("" + id);
+      
       if (errorEntry != null && isAvailable(errorEntry, level, version)) {
         SBMLError e = new SBMLError();
+        
         e.setCode(id);
         e.setCategory((String) errorEntry.get(JSON_KEY_CATEGORY));
+        e.setPackage((String) errorEntry.get(JSON_KEY_PACKAGE));
+        
         Message m = new Message();
         m.setMessage((String) errorEntry.get(JSON_KEY_MESSAGE));
+        m.setLang("en");
         e.setMessage(m);
+        
         Message sm = new Message();
         sm.setMessage((String) errorEntry.get(JSON_KEY_SHORT_MESSAGE));
+        sm.setLang("en");
         e.setShortMessage(sm);
-        e.setPackage((String) errorEntry.get(JSON_KEY_PACKAGE));
-        Object sev =
-          errorEntry.get(SBMLErrorFactory.getSeverityKey(level, version));
+        
+        Object sev = errorEntry.get(SBMLErrorFactory.getSeverityKey(level, version));
+        
         if (sev == null) {
           sev = errorEntry.get(SBMLErrorFactory.JSON_KEY_DEFAULT_SEVERITY);
         }
         e.setSeverity((String) sev);
-        // System.out.println("Out: " + e + " " + e.getShortMessage());
+
         return e;
       }
     }
@@ -97,8 +141,17 @@ public class SBMLErrorFactory {
   }
 
 
+  /**
+   * Returns {@code true} if the error is available for the given SBML level and version.
+   * 
+   * @param error the json SBML error object
+   * @param level the SBML level
+   * @param version the SBML version
+   * @return {@code true} if the error is available for the given SBML level and version.
+   */
   private static boolean isAvailable(JSONObject error, int level, int version) {
     String minLv = (String) (error.get(JSON_KEY_AVAILABLE));
+    
     if (minLv != null) {
 
       // Kicks out the first Character (because it will always be a 'L')
@@ -117,10 +170,18 @@ public class SBMLErrorFactory {
         return (level > l) || (level == l && version >= v);
       }
     }
+    
     return true;
   }
 
 
+  /**
+   * Gets the severity json key for a given SBML level and version.
+   * 
+   * @param level the SBML level
+   * @param version the SBML version
+   * @return the severity json key for a given SBML level and version.
+   */
   private static String getSeverityKey(int level, int version) {
     return String.format(JSON_KEY_UNFORMATED_SEVERITY, level, version);
   }
