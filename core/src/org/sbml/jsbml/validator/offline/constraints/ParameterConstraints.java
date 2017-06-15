b95209cb6b52316e53079c0d0cbda96968d12bba
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2017-06-15 16:00:41 +0100

    fixes for validation rules 80701 and 80702

--- core/src/org/sbml/jsbml/validator/offline/constraints/ParameterConstraints.java
+++ core/src/org/sbml/jsbml/validator/offline/constraints/ParameterConstraints.java
@@ -68,7 +68,7 @@ public class ParameterConstraints extends AbstractConstraintDeclaration {
     case MATHML_CONSISTENCY:
       break;
     case MODELING_PRACTICE:
-      if (level == 2 && context.isEnabledCategory(CHECK_CATEGORY.UNITS_CONSISTENCY)) {
+      if (context.isEnabledCategory(CHECK_CATEGORY.UNITS_CONSISTENCY)) {
         set.add(CORE_80701);
       }
       set.add(CORE_80702);
