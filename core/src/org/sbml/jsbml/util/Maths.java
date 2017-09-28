73b3711e5cd5d2d02c35b48be2e49495bf4797ce
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2017-09-28 11:19:15 +0100

    Added a method to return the avogadro constant depending of the SBML level and version in the Maths class

--- core/src/org/sbml/jsbml/util/Maths.java
+++ core/src/org/sbml/jsbml/util/Maths.java
@@ -22,7 +22,7 @@ package org.sbml.jsbml.util;
 import java.text.MessageFormat;
 
 /**
- * This class provides several static methods for mathematical operations such
+ * Provides several static methods for mathematical operations such
  * as faculty, logarithms and several trigonometric functions, which are not
  * part of standard Java, but necessary to evaluate the content of SBML files.
  * 
@@ -54,20 +54,20 @@ public class Maths {
   public static final double R = 8.314472;
 
   /**
-   * This method computes the arccosh of n
+   * Computes the arccosh of n
    * 
-   * @param n
-   * @return
+   * @param n a number
+   * @return the arccosh of n
    */
   public static final double arccosh(double n) {
     return Math.log(n + (Math.sqrt(Math.pow(n, 2) - 1)));
   }
 
   /**
-   * This method computes the arcus-cotangens of a double value.
+   * Computes the arcus-cotangens of a double value.
    * 
-   * @param x
-   * @return
+   * @param x a number
+   * @return the arcus-cotangens of a double value.
    */
   public static final double arccot(double x) {
     if (x == 0) {
@@ -81,10 +81,10 @@ public class Maths {
   }
 
   /**
-   * This method computes the arccoth of n
+   * Computes the arccoth of n
    * 
-   * @param n
-   * @return
+   * @param n a number
+   * @return the arccoth of n
    */
   public static final double arccoth(double n) {
     if (n == 0) {
@@ -94,10 +94,10 @@ public class Maths {
   }
 
   /**
-   * This method computes the arccosecant of a double value
+   * Computes the arccosecant of a double value
    * 
-   * @param x
-   * @return
+   * @param x a number
+   * @return the arccosecant of a double value
    */
   public static final double arccsc(double x) {
     if (x == 0) {
@@ -108,10 +108,10 @@ public class Maths {
   }
 
   /**
-   * This method computes the arccsch of n
+   * Computes the arccsch of n
    * 
-   * @param n
-   * @return
+   * @param n a number
+   * @return the arccsch of n
    */
   public static final double arccsch(double n) {
     if (n == 0) {
@@ -121,10 +121,10 @@ public class Maths {
   }
 
   /**
-   * This method computes the arcsecant of a double value
+   * Computes the arcsecant of a double value
    * 
-   * @param x
-   * @return
+   * @param x a number
+   * @return the arcsecant of a double value
    */
   public static final double arcsec(double x) {
     if (x == 0) {
@@ -135,10 +135,10 @@ public class Maths {
   }
 
   /**
-   * This method computes the arcsech of n
+   * Computes the arcsech of n
    * 
-   * @param n
-   * @return
+   * @param n a number
+   * @return the arcsech of n
    */
   public static final double arcsech(double n) {
     if (n == 0) {
@@ -148,30 +148,30 @@ public class Maths {
   }
 
   /**
-   * This method computes the arcsinh of n
+   * Computes the arcsinh of n
    * 
-   * @param n
-   * @return
+   * @param n a number
+   * @return the arcsinh of n
    */
   public static final double arcsinh(double n) {
     return Math.log(n + Math.sqrt(Math.pow(n, 2) + 1));
   }
 
   /**
-   * This method computes the arctanh of n
+   * Computes the arctanh of n
    * 
-   * @param n
-   * @return
+   * @param n a number
+   * @return the arctanh of n
    */
   public static final double arctanh(double n) {
     return (Math.log(1 + n) - Math.log(1 - n)) / 2;
   }
 
   /**
-   * This method computes the cot of n
+   * Computes the cot of n
    * 
-   * @param n
-   * @return
+   * @param n a number
+   * @return the cot of n
    */
   public static final double cot(double n) {
     double sin = Math.sin(n);
@@ -182,10 +182,10 @@ public class Maths {
   }
 
   /**
-   * This method computes the coth of n
+   * Computes the coth of n
    * 
-   * @param n
-   * @return
+   * @param n a number
+   * @return the coth of n
    */
   public static final double coth(double n) {
     double sinh = Math.sinh(n);
@@ -196,10 +196,10 @@ public class Maths {
   }
 
   /**
-   * This method computes the csc of n
+   * Computes the csc of n
    * 
-   * @param n
-   * @return
+   * @param n a number
+   * @return the csc of n
    */
   public static final double csc(double n) {
     double sin = Math.sin(n);
@@ -210,10 +210,10 @@ public class Maths {
   }
 
   /**
-   * This method computes the csch of n
+   * Computes the csch of n
    * 
-   * @param n
-   * @return
+   * @param n a number
+   * @return the csch of n
    */
   public static final double csch(double n) {
     double sinh = Math.sinh(n);
@@ -224,10 +224,10 @@ public class Maths {
   }
 
   /**
-   * This method computes the factorial! function.
+   * Computes the factorial! function.
    * 
-   * @param n
-   * @return
+   * @param n a number
+   * @return the factorial! function.
    */
   public static final long factorial(int n) {
     if (n < 0) {
@@ -244,8 +244,8 @@ public class Maths {
    * Checks if the given argument represents an integer number, i.e., if it can
    * be casted to int without loosing information.
    * 
-   * @param x
-   * @return
+   * @param x a number
+   * @return {@code true} if the given number represents an integer.
    */
   public static final boolean isInt(double x) {
     return x - ((int) x) == 0d;
@@ -254,31 +254,31 @@ public class Maths {
   // TODO: implement Gamma function for non-integer cases.
 
   /**
-   * This method computes the ln of n
+   * Computes the ln of n
    * 
-   * @param n
-   * @return
+   * @param n a number
+   * @return the ln of n
    */
   public static final double ln(double n) {
     return Math.log(n);
   }
 
   /**
-   * This method computes the log of n to the base 10
+   * Computes the log of n to the base 10
    * 
-   * @param n
-   * @return
+   * @param n a number
+   * @return the log of n to the base 10
    */
   public static final double log(double n) {
     return Math.log10(n);
   }
 
   /**
-   * This method computes the logarithm of a number x to a giving base b.
+   * Computes the logarithm of a number to a giving base.
    * 
-   * @param number
-   * @param base
-   * @return
+   * @param number a number
+   * @param base the base of the logarithm
+   * @return the logarithm of a number to a giving base.
    */
   public static final double log(double number, double base) {
     double denominator = Math.log(base);
@@ -289,11 +289,11 @@ public class Maths {
   }
 
   /**
-   * This method computes the rootExponent-th root of the radiant
+   * Computes the rootExponent-th root of the radiant
    * 
-   * @param radiant
-   * @param rootExponent
-   * @return
+   * @param radiant the radiant
+   * @param rootExponent the exponent
+   * @return the rootExponent-th root of the radiant
    */
   public static final double root(double radiant, double rootExponent) {
     if (rootExponent != 0) {
@@ -303,20 +303,20 @@ public class Maths {
   }
 
   /**
-   * This method computes the secant of a double value.
+   * Computes the secant of a double value.
    * 
-   * @param x
-   * @return
+   * @param x a number
+   * @return the secant of a double value.
    */
   public static final double sec(double x) {
     return 1d / Math.cos(x);
   }
 
   /**
-   * This method computes the sech of n
+   * Computes the sech of n
    * 
-   * @param n
-   * @return
+   * @param n a number
+   * @return the sech of n
    */
   public static final double sech(double n) {
     double cosh = Math.cosh(n);
@@ -331,6 +331,19 @@ public class Maths {
    * only.
    */
   private Maths() {
+  }
+
+  /**
+   * Returns the avogadro value corresponding to the given SBML level and version.
+   * 
+   * @param level the SBML level
+   * @param version the SBML version
+   * @return the avogadro value
+   */
+  public static final double getAvogadro(int level, int version) {
+    // TODO: If there is a different value in a later SBML specification, this must be checked here.
+
+    return AVOGADRO_L3V1;
   }
 
 }
