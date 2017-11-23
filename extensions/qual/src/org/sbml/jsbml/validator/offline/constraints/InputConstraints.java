7c58cdcb6635a71451d551663063c00a855c6e68
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2017-11-23 13:44:42 +0000

    comments and javadoc improvements

--- extensions/qual/src/org/sbml/jsbml/validator/offline/constraints/InputConstraints.java
+++ extensions/qual/src/org/sbml/jsbml/validator/offline/constraints/InputConstraints.java
@@ -1,3 +1,22 @@
+/*
+ * ----------------------------------------------------------------------------
+ * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
+ * for the latest version of JSBML and more information about SBML.
+ * 
+ * Copyright (C) 2009-2017 jointly by the following organizations:
+ * 1. The University of Tuebingen, Germany
+ * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ * 3. The California Institute of Technology, Pasadena, CA, USA
+ * 4. The University of California, San Diego, La Jolla, CA, USA
+ * 5. The Babraham Institute, Cambridge, UK
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation. A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution
+ * and also available online as <http://sbml.org/Software/JSBML/License>.
+ * ----------------------------------------------------------------------------
+ */
 package org.sbml.jsbml.validator.offline.constraints;
 
 import java.util.Set;
@@ -14,6 +33,12 @@ import org.sbml.jsbml.validator.offline.constraints.helper.UnknownCoreAttributeV
 import org.sbml.jsbml.validator.offline.constraints.helper.UnknownCoreElementValidationFunction;
 import org.sbml.jsbml.validator.offline.constraints.helper.UnknownPackageAttributeValidationFunction;
 
+/**
+ * Defines validation rules (as {@link ValidationFunction} instances) for the {@link Input} class.
+ * 
+ * @author Nicolas Rodriguez, Lisa Falk
+ * @since 1.3
+ */
 public class InputConstraints extends AbstractConstraintDeclaration {
 
 	@Override
@@ -100,8 +125,8 @@ public class InputConstraints extends AbstractConstraintDeclaration {
 		case QUAL_20505:
 			// The value of the attribute qual:sign of must conform the syntax of the SBML
 			// data type sign and may only take on the allowed values of sign defined in
-			// SBML; that is, the value must be one of the following: “positive”,
-			// “negative”, “dual” or “unknown”.
+			// SBML; that is, the value must be one of the following: 'positive',
+			// 'negative', 'dual' or 'unknown'.
 
 			func = new InvalidAttributeValidationFunction<Input>(QualConstants.sign);
 			break;
@@ -110,7 +135,7 @@ public class InputConstraints extends AbstractConstraintDeclaration {
 			// The value of the attribute qual:transitionEffect must conform to the
 			// syntax of the SBML data type transitionInputEffect and may only take on the
 			// allowed values of transitionInputEffect defined in SBML; that is, the value
-			// must be one of the following: “none” or “consumption”.
+			// must be one of the following: 'none' or 'consumption'.
 
 			func = new InvalidAttributeValidationFunction<Input>(QualConstants.transitionEffect);
 			break;
@@ -138,8 +163,8 @@ public class InputConstraints extends AbstractConstraintDeclaration {
 
 		case QUAL_20509:
 			// An Input that refers to a QualitativeSpecies that has a qual:constant
-			// attribute set to “true” cannot have the attribute qual:transitionEffect set
-			// to “consumption”.
+			// attribute set to 'true' cannot have the attribute qual:transitionEffect set
+			// to 'consumption'.
 		  
 		  func = new ValidationFunction<Input>() {
 		    @Override
