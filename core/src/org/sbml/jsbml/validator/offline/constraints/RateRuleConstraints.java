7a06e6b387535335d1f65635c87398e9f6d244ca
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2017-11-28 15:44:47 +0000

    Implemented constraints 10531-50534.

--- core/src/org/sbml/jsbml/validator/offline/constraints/RateRuleConstraints.java
+++ core/src/org/sbml/jsbml/validator/offline/constraints/RateRuleConstraints.java
@@ -21,7 +21,13 @@ package org.sbml.jsbml.validator.offline.constraints;
 
 import java.util.Set;
 
+import org.sbml.jsbml.Compartment;
+import org.sbml.jsbml.Model;
+import org.sbml.jsbml.Parameter;
 import org.sbml.jsbml.RateRule;
+import org.sbml.jsbml.Species;
+import org.sbml.jsbml.SpeciesReference;
+import org.sbml.jsbml.UnitDefinition;
 import org.sbml.jsbml.Variable;
 import org.sbml.jsbml.validator.SBMLValidator.CHECK_CATEGORY;
 import org.sbml.jsbml.validator.offline.ValidationContext;
@@ -68,6 +74,7 @@ public class RateRuleConstraints extends AbstractConstraintDeclaration {
     case SBO_CONSISTENCY:
       break;
     case UNITS_CONSISTENCY:
+      set.add(CORE_10531); // include 10532-10534
       break;
     }
   }
@@ -97,6 +104,63 @@ public class RateRuleConstraints extends AbstractConstraintDeclaration {
     ValidationFunction<RateRule> func = null;
 
     switch (errorCode) {
+      
+      case CORE_10531: // and 10532 and 10533 and 10534
+      {
+        func = new AbstractValidationFunction<RateRule>() {
+          
+          @Override
+          public boolean check(ValidationContext ctx, RateRule rateRule) {
+          
+            Variable variable = rateRule.getVariableInstance();
+            boolean check = true;
+            
+            if (variable != null) {
+              
+              UnitDefinition variableUnits = variable.getDerivedUnitDefinition();
+              
+              if (variableUnits != null && !variableUnits.isInvalid()) {
+                Model m = variable.getModel();
+                
+                variableUnits = variableUnits.clone().divideBy(m.getTimeUnitsInstance());
+                
+                if (!variableUnits.isInvalid()) {
+                  UnitDefinition ruleUnits = rateRule.getDerivedUnitDefinition();
+
+                  if (ruleUnits != null && !ruleUnits.isInvalid()) {
+                    
+                    check = UnitDefinition.areEquivalent(variableUnits, ruleUnits);
+
+                    if (!check) {
+                      int errorCode = -1;
+                      
+                      if (variable instanceof Compartment) { // 10531
+                        errorCode = CORE_10531;
+                      } 
+                      else if (variable instanceof Species) { // 10532
+                        errorCode = CORE_10532;
+                      }
+                      else if (variable instanceof Parameter) { // 10533
+                        errorCode = CORE_10533;
+                      }
+                      else if (variable instanceof SpeciesReference) { // 10534
+                        errorCode = CORE_10534;
+                      }
+                      
+                      ValidationConstraint.logError(ctx, errorCode, variable.getId());
+                    }
+                  }
+                }
+              }
+            }
+            
+            return check;
+          }
+        };
+        
+        break;
+      }
+ 
     case CORE_20902:
       func = new ValidationFunction<RateRule>() {
 
