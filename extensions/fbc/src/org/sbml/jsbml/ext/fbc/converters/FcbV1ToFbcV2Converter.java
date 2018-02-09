df8ac1d0309a9e830bcc63bcaf0c33a0d02e4e7d
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2018-02-09 16:35:46 +0000

    Several fixes for the FBC converters.

--- extensions/fbc/src/org/sbml/jsbml/ext/fbc/converters/FcbV1ToFbcV2Converter.java
+++ extensions/fbc/src/org/sbml/jsbml/ext/fbc/converters/FcbV1ToFbcV2Converter.java
@@ -26,6 +26,7 @@ import java.util.Properties;
 
 import javax.xml.stream.XMLStreamException;
 
+import org.sbml.jsbml.AbstractSBase;
 import org.sbml.jsbml.Model;
 import org.sbml.jsbml.Parameter;
 import org.sbml.jsbml.Reaction;
@@ -34,6 +35,7 @@ import org.sbml.jsbml.SBMLException;
 import org.sbml.jsbml.SBMLReader;
 import org.sbml.jsbml.SBase;
 import org.sbml.jsbml.TidySBMLWriter;
+import org.sbml.jsbml.ext.AbstractSBasePlugin;
 import org.sbml.jsbml.ext.SBasePlugin;
 import org.sbml.jsbml.ext.fbc.FBCConstants;
 import org.sbml.jsbml.ext.fbc.FBCModelPlugin;
@@ -44,12 +46,13 @@ import org.sbml.jsbml.util.converters.SBMLConverter;
 import org.sbml.jsbml.util.filters.Filter;
 
 /**
- * converts SBML FBCV1 files (that were created with the CobraToFbcV1Converter) to SBML FBCV2
+ * Converts SBML FBC Version 1 files to SBML FBC Version 2.
  * 
  * @author Thomas Hamm
  * @author Nicolas Rodriguez
  * @since 1.3
  */
+@SuppressWarnings("deprecation")
 public class FcbV1ToFbcV2Converter implements SBMLConverter {
 
   /* (non-Javadoc)
@@ -60,37 +63,70 @@ public class FcbV1ToFbcV2Converter implements SBMLConverter {
   public SBMLDocument convert(SBMLDocument sbmlDocument) throws SBMLException {
     if ( sbmlDocument.isPackageEnabled(FBCConstants.getNamespaceURI(3, 1, 1))) { 
   
-  // set SBMLDocument to fbcV2
+      
+      // TODO - it would probably be useful to have an utility method to do all of that automatically
+      sbmlDocument.enablePackage(FBCConstants.getNamespaceURI(3, 1, 1), false);
+      sbmlDocument.enablePackage(FBCConstants.getNamespaceURI(3, 1, 2));
+      
+      // set SBMLDocument to fbcV2
+      // settings first the fbc plugins package version to avoid exception in org.sbml.jsbml.AbstractSBase.setPackageVersion
       sbmlDocument.filter(new Filter() {
         
         @Override
         public boolean accepts(Object o) {
           if (o instanceof SBase) {
             SBase sBase = (SBase) o;
-            if (sBase.getPackageName() == "fbc") {
-              sBase.enablePackage(FBCConstants.getNamespaceURI(3, 1, 1), false);
-              sBase.enablePackage(FBCConstants.getNamespaceURI(3, 1, 2));
+            
+            if (sBase.getNumPlugins() > 0) {
+              SBasePlugin sBasePlugin = sBase.getPlugin("fbc");
+              
+              if (sBasePlugin != null) {
+                if (sBasePlugin.getPackageName().equals("fbc")) {
+                  sBasePlugin.setPackageVersion(2);
+                }
+                if (! sBasePlugin.getElementNamespace().equals(FBCConstants.namespaceURI_L3V1V2)) {
+                  ((AbstractSBasePlugin) sBasePlugin).setNamespace(null);
+                  ((AbstractSBasePlugin) sBasePlugin).setNamespace(FBCConstants.namespaceURI_L3V1V2);
+                }                
+              }
+            }            
+          }
+          
+          return false;
+        }
+      });
+
+      // settings the fbc SBase package version to avoid exception 
+      sbmlDocument.filter(new Filter() {
+        
+        @Override
+        public boolean accepts(Object o) {
+          if (o instanceof SBase) {
+            SBase sBase = (SBase) o;
+            if (sBase.getPackageName().equals("fbc")) {
+              if (sBase.getPackageVersion() != 2) {
+                sBase.setPackageVersion(2);
+              }
+              if (! sBase.getNamespace().equals(FBCConstants.namespaceURI_L3V1V2)) {
+                ((AbstractSBase) sBase).setNamespace(null);
+                ((AbstractSBase) sBase).setNamespace(FBCConstants.namespaceURI_L3V1V2);
+              }
             }
           }
       
-          if (o instanceof SBasePlugin) {
-            SBasePlugin sBasePlugin = (SBasePlugin) o;
-            if (sBasePlugin.getPackageName() == "fbc") {
-              sBasePlugin.setPackageVersion(2);
-            }
-          }
           return false;
         }
       });
       
-  // set fbc:strict = true
+      // set fbc:strict = true
+      // TODO - check how to set the strict attribute
       Model model = sbmlDocument.getModel();
       FBCModelPlugin fbcModelPlugin = (FBCModelPlugin)model.getPlugin("fbc");
       if (!fbcModelPlugin.isSetStrict()) {
         fbcModelPlugin.setStrict(true);
       }
       
-  // add lower and upper flux bounds to the reactions; add flux bounds to list of parameters; delete the list of flux bounds 
+      // add lower and upper flux bounds to the reactions; add flux bounds to list of parameters; delete the list of flux bounds 
       for (Reaction reaction : model.getListOfReactions()) {
         FBCReactionPlugin fbcReactionPlugin = (FBCReactionPlugin)reaction.getPlugin("fbc");
         List<FluxBound> fluxBList = new ArrayList<FluxBound>();
@@ -122,24 +158,38 @@ public class FcbV1ToFbcV2Converter implements SBMLConverter {
         }
       }
 
-    // notes ->fbc: geneProductAssociation in reactions and fbc:list of  Gene Products
+      // notes ->fbc: geneProductAssociation in reactions and fbc:list of  Gene Products
       Properties pElementsNote = new Properties();
-      int i = 1;
+
       for (Reaction reaction : model.getListOfReactions()) {
         pElementsNote = CobraUtil.parseCobraNotes(reaction);
-//       System.out.println(i);
-//       System.out.println(pElementsNote.getProperty("GENE_ASSOCIATION"));
-//       i=i+1;
+        String geneAssociation = null;
+
         if (pElementsNote.getProperty("GENE_ASSOCIATION") != null) {
-          GPRParser.parseGPR(reaction, pElementsNote.getProperty("GENE_ASSOCIATION"), false);
+          geneAssociation = pElementsNote.getProperty("GENE_ASSOCIATION");
+        } else if (pElementsNote.getProperty("GENE ASSOCIATION") != null) {
+          geneAssociation = pElementsNote.getProperty("GENE ASSOCIATION");
+        } 
+        // TODO - do we need to test more forms of 'GENE_ASSOCIATION'
+
+        if (geneAssociation != null && geneAssociation.trim().length() > 0) {
+          GPRParser.parseGPR(reaction, geneAssociation, false, false);
         }
       }
-    } 
+    }
+    
     return sbmlDocument;
   }
 
+  /**
+   * Converts SBML FBV V1 files to SBML FBC_V2.
+   * 
+   * @param args the arguments, expect two files path, input and output files.
+   * @throws XMLStreamException if an error occurs
+   * @throws IOException if an error occurs
+   */
   public static void main(String[] args) throws XMLStreamException, IOException {
-    // TODO: move this to the examples package!
+    // TODO: move/copy this to the examples package!
     // read document
     SBMLReader sbmlReader = new SBMLReader();
     SBMLDocument doc = sbmlReader.readSBMLFromFile(args[0]);
