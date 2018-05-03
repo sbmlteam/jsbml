7050bdcc72a71d458cb6f904e07f7fa0939b5b61
Thomas M. Hamm <hamm@informatik.uni-tuebingen.de>
2018-05-03 21:30:13 +0200

    class to test the FcbV1ToFbcV2Converter

---
+++ extensions/fbc/src/org/sbml/jsbml/ext/fbc/converters/TestFcbV1ToFbcV2Converter.java
@@ -0,0 +1,55 @@
+/*
+ * ---------------------------------------------------------------------------- 
+ * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML> 
+ * for the latest version of JSBML and more information about SBML. 
+ * 
+ * Copyright (C) 2009-2018 jointly by the following organizations: 
+ * 1. The University of Tuebingen, Germany 
+ * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK 
+ * 3. The California Institute of Technology, Pasadena, CA, USA 
+ * 4. The University of California, San Diego, La Jolla, CA, USA
+ * 5. The Babraham Institute, Cambridge, UK
+ * 
+ * This library is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published by 
+ * the Free Software Foundation. A copy of the license agreement is provided 
+ * in the file named "LICENSE.txt" included with this software distribution 
+ * and also available online as <http://sbml.org/Software/JSBML/License>. 
+ * ---------------------------------------------------------------------------- 
+ */
+package org.sbml.jsbml.ext.fbc.converters;
+
+import java.io.IOException;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.sbml.jsbml.SBMLDocument;
+import org.sbml.jsbml.SBMLReader;
+import org.sbml.jsbml.TidySBMLWriter;
+
+/**
+ * tests FbcV1ToFbcV2Converter
+ * 
+ * @author Thomas Hamm
+ * @author Nicolas Rodriguez
+ * @since 1.3
+ */
+public class TestFcbV1ToFbcV2Converter {
+
+  /**
+   * Converts SBML FBV_V1 files to SBML FBC_V2.
+   * 
+   * @param args the arguments, expect two files path, input and output files.
+   * @throws XMLStreamException if an error occurs
+   * @throws IOException if an error occurs
+   */
+  public static void main(String[] args) throws XMLStreamException, IOException {
+    // read document
+    SBMLReader sbmlReader = new SBMLReader();
+    SBMLDocument doc = sbmlReader.readSBMLFromFile(args[0]);
+    // convert and write document
+    FcbV1ToFbcV2Converter fcbV1ToFbcV2Converter = new FcbV1ToFbcV2Converter();
+    TidySBMLWriter tidySBMLWriter = new TidySBMLWriter();
+    tidySBMLWriter.writeSBMLToFile(fcbV1ToFbcV2Converter.convert(doc),args[1]);
+  }
+}
