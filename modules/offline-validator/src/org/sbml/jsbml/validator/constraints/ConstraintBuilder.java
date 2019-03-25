4c21792d078c40904eb5cf67979cd5cbb7d8dfb4
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2019-03-25 22:14:02 +0000

    removed the unused offline-validator module. Everything has been moved to jabml core.

--- modules/offline-validator/src/org/sbml/jsbml/validator/constraints/ConstraintBuilder.java
+++
@@ -1,6 +0,0 @@
-package org.sbml.jsbml.validator.constraints;
-
-public interface ConstraintBuilder
-{
-    public AnyConstraint<?> createConstraint(int id);
-}
--- modules/offline-validator/src/org/sbml/jsbml/validator/constraints/ValidationFunction.java
+++
@@ -1,8 +0,0 @@
-package org.sbml.jsbml.validator.constraints;
-
-import org.sbml.jsbml.validator.ValidationContext;
-
-public interface ValidationFunction<T>
-{
-  public boolean check(ValidationContext ctx, T t);
-}
