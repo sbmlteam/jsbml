5300b756016c47c7fc3ed1cc34c56b820fee56de
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2019-03-28 18:48:59 +0000

    Fixed new files header.

--- extensions/comp/src/org/sbml/jsbml/validator/offline/constraints/DeletionConstraints.java
+++ extensions/comp/src/org/sbml/jsbml/validator/offline/constraints/DeletionConstraints.java
@@ -7,8 +7,7 @@
  * 1. The University of Tuebingen, Germany
  * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
  * 3. The California Institute of Technology, Pasadena, CA, USA
- * 4. The University of California, San Diego, La Jolla, CA, USA
- * 5. The Babraham Institute, Cambridge, UK
+ * 4. The Babraham Institute, Cambridge, UK
  * 
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
