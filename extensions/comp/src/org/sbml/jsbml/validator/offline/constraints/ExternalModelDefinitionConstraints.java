ce3817465bced9c3ac2332eab1a5a0933b8724fe
Nicolas Rodriguez <niko.rodrigue@gmail.com>
2019-03-28 19:23:44 +0000

    Implemented some of the ExternalModelDefinition constraints

--- extensions/comp/src/org/sbml/jsbml/validator/offline/constraints/ExternalModelDefinitionConstraints.java
+++ extensions/comp/src/org/sbml/jsbml/validator/offline/constraints/ExternalModelDefinitionConstraints.java
@@ -20,9 +20,14 @@ package org.sbml.jsbml.validator.offline.constraints;
 
 import java.util.Set;
 
+import org.sbml.jsbml.ext.comp.CompConstants;
 import org.sbml.jsbml.ext.comp.ExternalModelDefinition;
 import org.sbml.jsbml.validator.SBMLValidator.CHECK_CATEGORY;
-import org.sbml.jsbml.validator.offline.ValidationContext;;
+import org.sbml.jsbml.validator.SyntaxChecker;
+import org.sbml.jsbml.validator.offline.ValidationContext;
+import org.sbml.jsbml.validator.offline.constraints.helper.UnknownCoreAttributeValidationFunction;
+import org.sbml.jsbml.validator.offline.constraints.helper.UnknownElementValidationFunction;
+import org.sbml.jsbml.validator.offline.constraints.helper.UnknownPackageAttributeValidationFunction;;
 
 /**
  * Defines validation rules (as {@link ValidationFunction} instances) for the {@link ExternalModelDefinition} class.
@@ -78,53 +83,89 @@ public class ExternalModelDefinitionConstraints extends AbstractConstraintDeclar
 
     switch (errorCode) {
 
-    case COMP_20301: // 
+    case COMP_20301:
     {
+      func = new UnknownCoreAttributeValidationFunction<ExternalModelDefinition>();
+      break;
+    }
+    case COMP_20302:
+    {
+      func = new UnknownElementValidationFunction<ExternalModelDefinition>();
+      break;
+    }
+    case COMP_20303:
+    {
+      func = new ValidationFunction<ExternalModelDefinition>() {
+
+        @Override
+        public boolean check(ValidationContext ctx, ExternalModelDefinition extM) {
+          
+          if (!extM.isSetId() || !extM.isSetSource()) {
+            return false;
+          }
+          
+          return new UnknownPackageAttributeValidationFunction<ExternalModelDefinition>(CompConstants.shortLabel).check(ctx, extM);
+        }
+      };
+      break;
+    }
+    case COMP_20304:
+    {
+      // The value of the source attribute on an ExternalModelDefinition object must reference
+      // an SBML Level 3 Version 1 document
       // TODO
       break;
     }
-    case COMP_20302: // 
+    case COMP_20305:
     {
+      // The value of the modelRef attribute on an ExternalModelDefinition object must be the
+      // value of an id attribute on a Model or ExternalModelDefinition object in the SBML document
+      // referenced by the source attribute
       // TODO
       break;
     }
-    case COMP_20303: // 
+    case COMP_20306:
     {
+      // TODO
+      // The value of the md5 attribute, if present on an ExternalModelDefinition object, should
+      // match the calculated MD5 checksum of the SBML document referenced by the source
+      // attribute.
+      break;
+    }
+    case COMP_20307:
+    {
+      // The value of a source attribute on an ExternalModelDefinition object must always conform to
+      // the syntax of the XML Schema 1.0 data type anyURI.
       // TODO
       break;
     }
-    case COMP_20304: // 
+    case COMP_20308:
     {
-      // TODO
+      func = new ValidationFunction<ExternalModelDefinition>() {
+
+        @Override
+        public boolean check(ValidationContext ctx, ExternalModelDefinition extM) {
+          
+          if (extM.isSetModelRef()) {
+            return SyntaxChecker.isValidId(extM.getModelRef(), ctx.getLevel(), ctx.getVersion());
+          }
+          
+          return true;
+        }
+      };
       break;
     }
-    case COMP_20305: // 
+    case COMP_20309:      
     {
-      // TODO
+      // The value of a md5 attribute on an ExternalModelDefinition object must always conform to the syntax of type string
+      // nothing to do.
       break;
     }
-    case COMP_20306: // 
+    case COMP_20310:
     {
-      // TODO
-      break;
-    }
-    case COMP_20307: // 
-    {
-      // TODO
-      break;
-    }
-    case COMP_20308: // 
-    {
-      // TODO
-      break;
-    }
-    case COMP_20309: // 
-    {
-      // TODO
-      break;
-    }
-    case COMP_20310: // 
-    {
+      // An ExternalModelDefinition object must not reference an ExternalModelDefinition in a different
+      // SBML document that, in turn, refers back to the original ExternalModelDefinition object,
+      // whether directly or indirectly through a chain of ExternalModelDefinition objects.
       // TODO
       break;
     }
