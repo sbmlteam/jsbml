37b2e0fac6eb3340aa5eb0c75697919bca4d00f6
Onur101010 <onuroezel@aol.de>
2019-11-10 02:57:42 +0100

    Created ValidationContextTest Class with a few initial tests

---
+++ core/test/org/sbml/jsbml/test/ValidationContextTest.java
@@ -0,0 +1,52 @@
+package org.sbml.jsbml.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sbml.jsbml.SBMLDocument;
+import org.sbml.jsbml.SBMLReader;
+import org.sbml.jsbml.test.sbml.TestReadFromFile5;
+import org.sbml.jsbml.util.ValuePair;
+import org.sbml.jsbml.validator.offline.ValidationContext;
+
+public class ValidationContextTest {
+	
+	/**
+	 * 
+	 */
+	private SBMLDocument doc;
+	
+	/**
+	 * 
+	 */
+	private ValidationContext ctx; 
+	
+	 /**
+	   * loads testfile with id BIOMD0000000228 
+	   * @throws IOException
+	   * @throws XMLStreamException
+	   */
+	  @Before public void setUp()  throws IOException, XMLStreamException{
+		  InputStream fileStream = TestReadFromFile5.class.getResourceAsStream("/org/sbml/jsbml/xml/test/data/l2v4/BIOMD0000000228.xml");
+		  doc = new SBMLReader().readSBMLFromStream(fileStream);
+		  ctx = new ValidationContext(doc.getLevel(),doc.getVersion()); //ctx with lvl and version of testdocument 
+	  }
+
+	  @Test
+	  public void getLevelTest() {
+		  assertTrue(ctx.getLevel() == doc.getLevel());
+	  }
+	  
+	  @Test
+	  public void getLevelAndVersionTest() {
+		  ValuePair<Integer, Integer> lv = ctx.getLevelAndVersion();
+		  assertTrue(lv.getL() == doc.getLevel());
+		  assertTrue(lv.getV() == doc.getVersion());
+	  }
+}
