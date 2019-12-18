79104b549dd598fba1da10ff3512ff77fb62c407
Onur101010 <onuroezel@aol.de>
2019-12-18 13:21:25 +0100

    some fixes and new tests

--- core/src/org/sbml/jsbml/validator/SyntaxChecker.java
+++ core/src/org/sbml/jsbml/validator/SyntaxChecker.java
@@ -221,6 +221,25 @@ public class SyntaxChecker {
      */
     return syntaxChecker.metaIdPattern.matcher(idCandidate).matches();
   }
+  
+  /**
+   * Checks if the given identifier candidate satisfies the requirements for a
+   * valid meta identifier (for any version and level).
+   *
+   * @param idCandidate, level, version
+   * @return {@code true} if the given argument is a valid meta identifier
+   *         {@link String}, {@code false} otherwise.
+   */
+  public static final boolean isValidMetaId(String idCandidate, int level, int version) {
+   
+    //TODO - was machen wenn es schon in MetaIdPattern gibt 
+    syntaxChecker.initMetaIdPatterns(level, version);
+    
+    if(syntaxChecker.metaIdPattern == null) {
+      return false;
+    }
+    return syntaxChecker.metaIdPattern.matcher(idCandidate).matches();
+  }
 
   /**
    * Definition of valid e-mail addresses. Initialized upon first use.
@@ -371,8 +390,9 @@ public class SyntaxChecker {
   
   //overloaded method with level and version to be clear which patterns should be initialized 
   private String initMetaIdPatterns(int level, int version) {
+    metaIdPattern = null;
     String underscore = "_";
-    String hyphen = "-";
+    String dash = "\\-";
     String dot = ".";
     String colon = ":";
 
@@ -384,47 +404,52 @@ public class SyntaxChecker {
       String sName;
       if(version == 1) {
         //see under specification for SBML L1V1: 3.3 Type SName 
-        sName = "^[" + underscore + "]*" + letter + "[" + letter + "|" + underscore + "|" + digit + "]*";
-
+        sName = underscore + "*" + "[" + letter + "]" + "[" + letter +  underscore + digit + "]*";
+        metaIdPattern = Pattern.compile(sName);
+        System.out.println("level 1, version 1: attempt to load patterns: " + sName);
         return sName;     
       } else if(version == 2) { 
         //see under specification for SBML L1V2: 3.3 Type SName 
-        sName = "^[" + letter + "|" + underscore + "]" + "[" + letter + "|" + digit + "|" + underscore + "]*";
-
+        sName = "[" + letter + underscore + "]" + "[" + letter + digit + underscore + "]*";
+        metaIdPattern = Pattern.compile(sName);
+        System.out.println("level 1, version 2: attempt to load patterns: " + sName);
         return sName; 
       }
       System.out.println("no valid version, could not load pattern correctly");
+      break;
     } 
 
     case 2: {  
-      if(version == 1) {      
+      if(version == 1) {  //TODO - gleich wie L1V2 also zusammenführen
         //Id has Type Sid, see Specification of SBML L2V1 under 3.4
         String letter = "a-zA-Z";
         String digit = "0-9";
-        String nameChar = "^[" + letter + "|" + digit + "|" + underscore + "]"; 
-        String name = "^[" + letter + "|" + underscore + "]" + "[" + nameChar + "]*";
-
+        String nameChar = letter + digit + underscore; 
+        String name = "[" + letter + underscore + "]" + "[" + nameChar + "]*";
+        metaIdPattern = Pattern.compile(name);
+        System.out.println("level 2, version 1: attempt to load patterns: " + name);
         return name; 
 
       } else if(version == 2) {     
-        //Id is TypeID, see Specification of SBML L2V2 under 3.1.6
+        //Id is TypeID, see Specification of SBML L2V2R1 under 3.1.6
         String letter = "a-zA-Z";
         String digit = "0-9";
-        StringBuilder combiningChar = StringTools.concatStringBuilder("[\u0300-\u0345]|[\u0360-\u0361]|[\u0483-\u0486]|[\u0591-\u05A1]|[\u05A3-\u05B9]|[\u05BB-\u05BD]"
-          , "|\u05BF|[\u05C1-\u05C2]|\u05C4|[\u064B-\u0652]|\u0670|[\u06D6-\u06DC]|[\u06DD-\u06DF]|[\u06E0-\u06E4]|[\u06E7-\u06E8]"
-          , "|[\u06EA-\u06ED]|[\u0901-\u0903]|\u093C|[\u093E-\u094C]|\u094D|[\u0951-\u0954]|[\u0962-\u0963]|[\u0981-\u0983]"
-          , "|\u09BC|\u09BE|\u09BF|[\u09C0-\u09C4]|[\u09C7-\u09C8]|[\u09CB-\u09CD]|\u09D7|[\u09E2-\u09E3]|\u0A02|\u0A3C|\u0A3E"
-          , "|\u0A3F|[\u0A40-\u0A42]|[\u0A47-\u0A48]|[\u0A4B-\u0A4D]|[\u0A70-\u0A71]|[\u0A81-\u0A83]|\u0ABC|[\u0ABE-\u0AC5]"
-          , "|[\u0AC7-\u0AC9]|[\u0ACB-\u0ACD]|[\u0B01-\u0B03]|\u0B3C|[\u0B3E-\u0B43]|[\u0B47-\u0B48]|[\u0B4B-\u0B4D]|[\u0B56-\u0B57]"
-          , "|[\u0B82-\u0B83]|[\u0BBE-\u0BC2]|[\u0BC6-\u0BC8]|[\u0BCA-\u0BCD]|\u0BD7|[\u0C01-\u0C03]|[\u0C3E-\u0C44]|[\u0C46-\u0C48]"
-          , "|[\u0C4A-\u0C4D]|[\u0C55-\u0C56]|[\u0C82-\u0C83]|[\u0CBE-\u0CC4]|[\u0CC6-\u0CC8]|[\u0CCA-\u0CCD]|[\u0CD5-\u0CD6]"
-          , "|[\u0D02-\u0D03]|[\u0D3E-\u0D43]|[\u0D46-\u0D48]|[\u0D4A-\u0D4D]|\u0D57|\u0E31|[\u0E34-\u0E3A]|[\u0E47-\u0E4E]"
-          , "|\u0EB1|[\u0EB4-\u0EB9]|[\u0EBB-\u0EBC]|[\u0EC8-\u0ECD]|[\u0F18-\u0F19]|\u0F35|\u0F37|\u0F39|\u0F3E|\u0F3F"
-          , "|[\u0F71-\u0F84]|[\u0F86-\u0F8B]|[\u0F90-\u0F95]|\u0F97|[\u0F99-\u0FAD]|[\u0FB1-\u0FB7]|\u0FB9|[\u20D0-\u20DC]|\u20E1|[\u302A-\u302F]|\u3099|\u309A");
-        String extender = "\u00B7|\u02D0|\u02D1|\u0387|\u0640|\u0E46|\u0EC6|\u3005|[\u3031-\u3035]|[\u309D-\u309E]|[\u30FC-\u30FE]";
-        String ncNameChar = "^[" + letter + "|" + digit + "|" + dot + "|" + hyphen + "|" + underscore + "|" + combiningChar + "|" + extender + "]";
-        String id = "^[" + letter + "|" + underscore + "]" + "[" + ncNameChar + "]*"; 
-
+        StringBuilder combiningChar = StringTools.concatStringBuilder("[\\u0300-\\u0345][\\u0360-\\u0361][\\u0483-\\u0486][\\u0591-\\u05A1][\\u05A3-\\u05B9][\\u05BB-\\u05BD]"
+          , "\\u05BF[\\u05C1-\\u05C2]\\u05C4[\\u064B-\\u0652]\\u0670[\\u06D6-\\u06DC][\\u06DD-\\u06DF][\\u06E0-\\u06E4][\\u06E7-\\u06E8]"
+          , "[\\u06EA-\\u06ED][\\u0901-\\u0903]\\u093C[\\u093E-\\u094C]\\u094D[\\u0951-\\u0954][\\u0962-\\u0963][\\u0981-\\u0983]"
+          , "\\u09BC\\u09BE\\u09BF[\\u09C0-\\u09C4][\\u09C7-\\u09C8][\\u09CB-\\u09CD]\\u09D7[\\u09E2-\\u09E3]\\u0A02\\u0A3C\\u0A3E"
+          , "\\u0A3F[\\u0A40-\\u0A42][\\u0A47-\\u0A48][\\u0A4B-\\u0A4D][\\u0A70-\\u0A71][\\u0A81-\\u0A83]\\u0ABC[\\u0ABE-\\u0AC5]"
+          , "[\\u0AC7-\\u0AC9][\\u0ACB-\\u0ACD][\\u0B01-\\u0B03]\\u0B3C[\\u0B3E-\\u0B43][\\u0B47-\\u0B48][\\u0B4B-\\u0B4D][\\u0B56-\\u0B57]"
+          , "[\\u0B82-\\u0B83][\\u0BBE-\\u0BC2][\\u0BC6-\\u0BC8][\\u0BCA-\\u0BCD]\\u0BD7[\\u0C01-\\u0C03][\\u0C3E-\\u0C44][\\u0C46-\\u0C48]"
+          , "[\\u0C4A-\\u0C4D][\\u0C55-\\u0C56][\\u0C82-\\u0C83][\\u0CBE-\\u0CC4][\\u0CC6-\\u0CC8][\\u0CCA-\\u0CCD][\\u0CD5-\\u0CD6]"
+          , "[\\u0D02-\\u0D03][\\u0D3E-\\u0D43][\\u0D46-\\u0D48][\\u0D4A-\\u0D4D]\\u0D57\\u0E31[\\u0E34-\\u0E3A][\\u0E47-\\u0E4E]"
+          , "\\u0EB1[\\u0EB4-\\u0EB9][\\u0EBB-\\u0EBC][\\u0EC8-\\u0ECD][\\u0F18-\\u0F19]\\u0F35\\u0F37\\u0F39\\u0F3E\\u0F3F"
+          , "[\\u0F71-\\u0F84][\\u0F86-\\u0F8B][\\u0F90-\\u0F95]\\u0F97[\\u0F99-\\u0FAD][\\u0FB1-\\u0FB7]\\u0FB9[\\u20D0-\\u20DC]\\u20E1[\\u302A-\\u302F]\\u3099\\u309A");
+        String extender = "\\u00B7\\u02D0\\u02D1\\u0387\\u0640\\u0E46\\u0EC6\\u3005[\\u3031-\\u3035][\\u309D-\\u309E][\\u30FC-\\u30FE]";
+        String ncNameChar = letter + digit  + dot  + dash + underscore + combiningChar + extender;
+        String id = "[" + letter + underscore + "]" + "[" + ncNameChar + "]*"; 
+        System.out.println("level 2, version 2: attempt to load patterns: " + id);
+        metaIdPattern = Pattern.compile(id);
         return id;     
 
       } else if(version == 3 || version == 4 || version == 5) {        
@@ -467,13 +492,14 @@ public class SyntaxChecker {
           , "|\u0EB1|[\u0EB4-\u0EB9]|[\u0EBB-\u0EBC]|[\u0EC8-\u0ECD]|[\u0F18-\u0F19]|\u0F35|\u0F37|\u0F39|\u0F3E|\u0F3F"
           , "|[\u0F71-\u0F84]|[\u0F86-\u0F8B]|[\u0F90-\u0F95]|\u0F97|[\u0F99-\u0FAD]|[\u0FB1-\u0FB7]|\u0FB9|[\u20D0-\u20DC]|\u20E1|[\u302A-\u302F]|\u3099|\u309A");
         String extender = "\u00B7|\u02D0|\u02D1|\u0387|\u0640|\u0E46|\u0EC6|\u3005|[\u3031-\u3035]|[\u309D-\u309E]|[\u30FC-\u30FE]";
-        String ncNameChar = "^[" + letter + "|" + digit + "|" + dot + "|" + hyphen + "|" + underscore + "|" + combiningChar + "|" + extender + "]";
+        String ncNameChar = "^[" + letter + "|" + digit + "|" + dot + "|" + dash + "|" + underscore + "|" + combiningChar + "|" + extender + "]";
         String id = "^[" + letter + "|" + underscore + "|" + colon + "]" + "[" + ncNameChar + "]*";    
-
+        metaIdPattern = Pattern.compile(id);
         return id;  
 
       }
       System.out.println("no valid version, could not load pattern correctly");
+      break;
     } 
 
     case 3: {      
@@ -517,13 +543,14 @@ public class SyntaxChecker {
           , "|\u0EB1|[\u0EB4-\u0EB9]|[\u0EBB-\u0EBC]|[\u0EC8-\u0ECD]|[\u0F18-\u0F19]|\u0F35|\u0F37|\u0F39|\u0F3E|\u0F3F"
           , "|[\u0F71-\u0F84]|[\u0F86-\u0F8B]|[\u0F90-\u0F95]|\u0F97|[\u0F99-\u0FAD]|[\u0FB1-\u0FB7]|\u0FB9|[\u20D0-\u20DC]|\u20E1|[\u302A-\u302F]|\u3099|\u309A");
         String extender = "\u00B7|\u02D0|\u02D1|\u0387|\u0640|\u0E46|\u0EC6|\u3005|[\u3031-\u3035]|[\u309D-\u309E]|[\u30FC-\u30FE]";
-        String ncNameChar = "^[" + letter + "|" + digit + "|" + dot + "|" + hyphen + "|" + underscore + "|" + combiningChar + "|" + extender + "]";
+        String ncNameChar = "^[" + letter + "|" + digit + "|" + dot + "|" + dash + "|" + underscore + "|" + combiningChar + "|" + extender + "]";
         String id = "^[" + letter + "|" + underscore + "|" + colon + "]" + "[" + ncNameChar + "]*";
-
+        metaIdPattern = Pattern.compile(id);
         return id; 
 
       }
       System.out.println("no valid or unknown version, could not load pattern correctly");
+      break;
       //when new version comes method should be updated 
     }
     }
