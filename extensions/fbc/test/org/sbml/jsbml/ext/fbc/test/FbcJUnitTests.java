ff228c4f4b5881680138d85f00ee820edca0a7eb
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2020-02-26 15:17:32 +0100

    TestSuite Base for Fbc extension

---
+++ extensions/fbc/test/org/sbml/jsbml/ext/fbc/test/FbcJUnitTests.java
@@ -0,0 +1,11 @@
+package org.sbml.jsbml.ext.fbc.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(value=Suite.class)
+@SuiteClasses(value={FbcV2ToCobraConverterTest.class})
+public class FbcJUnitTests {
+
+}
