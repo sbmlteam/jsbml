242db3ab08b5118a4b8232e468fe52f60d86acd7
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2020-02-26 15:24:11 +0100

    added missing file header

--- extensions/fbc/test/org/sbml/jsbml/ext/fbc/test/FbcJUnitTests.java
+++ extensions/fbc/test/org/sbml/jsbml/ext/fbc/test/FbcJUnitTests.java
@@ -1,9 +1,35 @@
+/*
+ * ----------------------------------------------------------------------------
+ * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
+ * for the latest version of JSBML and more information about SBML.
+ *
+ * Copyright (C) 2009-2020 jointly by the following organizations:
+ * 1. The University of Tuebingen, Germany
+ * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ * 3. The California Institute of Technology, Pasadena, CA, USA
+ * 4. The University of California, San Diego, La Jolla, CA, USA
+ * 5. The Babraham Institute, Cambridge, UK
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation. A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution
+ * and also available online as <http://sbml.org/Software/JSBML/License>.
+ * ----------------------------------------------------------------------------
+ */
 package org.sbml.jsbml.ext.fbc.test;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+
+/**
+ * Suite of tests for the fbc package.
+ * 
+ * @author Thorsten Tiede
+ * @since 1.5
+ */
 @RunWith(value=Suite.class)
 @SuiteClasses(value={FbcV2ToCobraConverterTest.class})
 public class FbcJUnitTests {
