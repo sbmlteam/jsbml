8442d0c930ccb3a394643115713f364a9f1eedc3
Onur101010 <onuroezel@aol.de>
2020-02-28 20:44:38 +0100

    Revert "added abstract class"
    This reverts commit e817289a244917565bf7f26307a149168998e63a.

--- extensions/layout/test/org/sbml/jsbml/ext/layout/test/PackageDisablerTests.java
+++ extensions/layout/test/org/sbml/jsbml/ext/layout/test/PackageDisablerTests.java
@@ -13,14 +13,13 @@ import org.sbml.jsbml.Species;
 import org.sbml.jsbml.SpeciesReference;
 import org.sbml.jsbml.ext.SBasePlugin;
 import org.sbml.jsbml.ext.layout.LayoutConstants;
-import org.sbml.jsbml.test.PackageDisablerTests;
 import org.sbml.jsbml.util.PackageDisabler;
 
 /**
  * @author Onur &Ouml;zel
  * @since 1.5
  */
-public class LayoutPackageDisablerTests extends PackageDisablerTests{
+public class PackageDisablerTests {
   //TODO- make this tests abstract and add them to the packages 
   private SBMLDocument doc; 
   private Model m;
