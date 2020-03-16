94d51b6fb4b0884303773eca08abea33eff139df
DEVetter <davidemanuelvetter@gmail.com>
2020-03-16 12:45:57 +0100

    GlobalRenderInformation, GlobalStyle

---
+++ extensions/render/src/org/sbml/jsbml/validator/offline/constraints/GlobalStyleConstraint.java
@@ -0,0 +1,72 @@
+/*
+ * ----------------------------------------------------------------------------
+ * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
+ * for the latest version of JSBML and more information about SBML.
+ *
+ * Copyright (C) 2009-2018 jointly by the following organizations:
+ * 1. The University of Tuebingen, Germany
+ * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ * 3. The California Institute of Technology, Pasadena, CA, USA
+ * 4. The University of California, San Diego, La Jolla, CA, USA
+ * 5. The Babraham Institute, Cambridge, UK
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation. A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution
+ * and also available online as <http://sbml.org/Software/JSBML/License>.
+ * ----------------------------------------------------------------------------
+ */
+package org.sbml.jsbml.validator.offline.constraints;
+
+import java.util.Set;
+
+import org.sbml.jsbml.ext.render.Style;
+import org.sbml.jsbml.validator.SBMLValidator.CHECK_CATEGORY;
+import org.sbml.jsbml.validator.offline.ValidationContext;
+import org.sbml.jsbml.validator.offline.constraints.helper.UnknownCoreAttributeValidationFunction;
+import org.sbml.jsbml.validator.offline.constraints.helper.UnknownCoreElementValidationFunction;
+
+/**
+ * Defines validation rules (as {@link ValidationFunction} instances) for the
+ * {@link Style} class.
+ * 
+ * @author David Emanuel Vetter
+ */
+public class GlobalStyleConstraint extends AbstractConstraintDeclaration {
+
+  @Override
+  public void addErrorCodesForCheck(Set<Integer> set, int level, int version,
+    CHECK_CATEGORY category, ValidationContext context) {
+    switch(category) {
+    case GENERAL_CONSISTENCY:
+      addRangeToSet(set, RENDER_20801, RENDER_20802);
+      break;
+    default:
+      break;
+    }
+  }
+
+
+  @Override
+  public void addErrorCodesForAttribute(Set<Integer> set, int level,
+    int version, String attributeName, ValidationContext context) {
+    // TODO Auto-generated method stub
+  }
+
+
+  @Override
+  public ValidationFunction<?> getValidationFunction(int errorCode,
+    ValidationContext context) {
+    ValidationFunction<Style> func = null;
+    switch(errorCode) {
+    case RENDER_20801:
+      func = new UnknownCoreAttributeValidationFunction<Style>();
+      break;
+    case RENDER_20802:
+      func = new UnknownCoreElementValidationFunction<Style>();
+      break;
+    }
+    return func;
+  }
+}
