32b1084166caccb330d6c26a8e50d23b5de033e1
dyrpsf <dyrpsf@gmail.com>
2026-03-09 21:16:08 +0530

    Refactor author order test to use helper method and SBML resource file (#219)

--- core/test/org/sbml/jsbml/xml/parsers/AuthorOrderRoundTripTest.java
+++ core/test/org/sbml/jsbml/xml/parsers/AuthorOrderRoundTripTest.java
@@ -1,8 +1,10 @@
 package org.sbml.jsbml.xml.parsers;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
+import java.io.InputStream;
 
 import org.junit.Test;
 import org.sbml.jsbml.Creator;
@@ -18,56 +20,27 @@ import org.sbml.jsbml.SBMLWriter;
  */
 public class AuthorOrderRoundTripTest {
 
-  private static final String SBML_WITH_CREATORS =
-      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-      "<sbml xmlns=\"http://www.sbml.org/sbml/level2/version4\" level=\"2\" version=\"4\">\n" +
-      "  <model id=\"m\">\n" +
-      "    <annotation>\n" +
-      "      <rdf:RDF\n" +
-      "          xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n" +
-      "          xmlns:dc=\"http://purl.org/dc/elements/1.1/\"\n" +
-      "          xmlns:vCard=\"http://www.w3.org/2001/vcard-rdf/3.0#\">\n" +
-      "        <rdf:Description rdf:about=\"#m\">\n" +
-      "          <dc:creator>\n" +
-      "            <rdf:Bag>\n" +
-      "              <rdf:li rdf:parseType=\"Resource\">\n" +
-      "                <vCard:N rdf:parseType=\"Resource\">\n" +
-      "                  <vCard:Family>Family1</vCard:Family>\n" +
-      "                  <vCard:Given>Given1</vCard:Given>\n" +
-      "                </vCard:N>\n" +
-      "              </rdf:li>\n" +
-      "              <rdf:li rdf:parseType=\"Resource\">\n" +
-      "                <vCard:N rdf:parseType=\"Resource\">\n" +
-      "                  <vCard:Family>Family2</vCard:Family>\n" +
-      "                  <vCard:Given>Given2</vCard:Given>\n" +
-      "                </vCard:N>\n" +
-      "              </rdf:li>\n" +
-      "              <rdf:li rdf:parseType=\"Resource\">\n" +
-      "                <vCard:N rdf:parseType=\"Resource\">\n" +
-      "                  <vCard:Family>Family3</vCard:Family>\n" +
-      "                  <vCard:Given>Given3</vCard:Given>\n" +
-      "                </vCard:N>\n" +
-      "              </rdf:li>\n" +
-      "            </rdf:Bag>\n" +
-      "          </dc:creator>\n" +
-      "        </rdf:Description>\n" +
-      "      </rdf:RDF>\n" +
-      "    </annotation>\n" +
-      "  </model>\n" +
-      "</sbml>\n";
+  private static final String RESOURCE = "author-order-example.xml";
+
+  private void assertCreatorsInExpectedOrder(java.util.List<org.sbml.jsbml.Creator> creators) {
+    assertEquals(3, creators.size());
+    assertEquals("Given1 Family1", creators.get(0).printCreator());
+    assertEquals("Given2 Family2", creators.get(1).printCreator());
+    assertEquals("Given3 Family3", creators.get(2).printCreator());
+  }
 
   @Test
   public void preservesCreatorOrderAfterRoundTrip() throws Exception {
-    // First read
-    SBMLDocument doc1 = new SBMLReader().readSBMLFromString(SBML_WITH_CREATORS);
+    SBMLDocument doc1;
+    try (InputStream is = AuthorOrderRoundTripTest.class.getResourceAsStream(RESOURCE)) {
+      assertNotNull("Test SBML resource not found: " + RESOURCE, is);
+      doc1 = new SBMLReader().readSBMLFromStream(is);
+    }
+
     Model m1 = doc1.getModel();
     History h1 = m1.getHistory();
     List<Creator> creators1 = h1.getListOfCreators();
-
-    assertEquals(3, creators1.size());
-    assertEquals("Given1 Family1", creators1.get(0).printCreator());
-    assertEquals("Given2 Family2", creators1.get(1).printCreator());
-    assertEquals("Given3 Family3", creators1.get(2).printCreator());
+    assertCreatorsInExpectedOrder(creators1);
 
     // Write back to string
     String xml = new SBMLWriter().writeSBMLToString(doc1);
@@ -77,10 +50,6 @@ public class AuthorOrderRoundTripTest {
     Model m2 = doc2.getModel();
     History h2 = m2.getHistory();
     List<Creator> creators2 = h2.getListOfCreators();
-
-    assertEquals(3, creators2.size());
-    assertEquals("Given1 Family1", creators2.get(0).printCreator());
-    assertEquals("Given2 Family2", creators2.get(1).printCreator());
-    assertEquals("Given3 Family3", creators2.get(2).printCreator());
+    assertCreatorsInExpectedOrder(creators2);
   }
 }
